c9796be5cb62c24b9574d7c4eb45ad217837d776
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-12-10 16:06:08 +0100

    based on the most recent spreadsheet in gdoc the list is 'physical', but we now expect in column 'D' the index of the slot where the node is currently deployed -- from that information we generate fit.json that makes the distinction between the 'physical' index (the number on the printed label) and the 'logical' index (the slot in the lab where the node is actually deployed
    of course it would make sense to minimize shuffling things around, but this version should let us fill in the 4 gaps (and there is provision for that in this version, with e.g. phy38 assigned to slot 15, and so on)

--- inventory/fit_to_json.py
+++ inventory/fit_to_json.py
@@ -1,42 +1,45 @@
 #!/usr/bin/env python
 
 class Node(object):
-    def __init__(self, number, mac, alt_mac):
-        self.number = number
+    def __init__(self, phy_num, log_num, mac, alt_mac):
+        self.phy_num = phy_num
+        self.log_num = log_num
         self.mac = mac
         self.alt_mac = alt_mac
 
     def model(self):
-        node2 = "{:02d}".format(self.number)
-        nodename = "fit"+node2
+        phy2 = "{:02d}".format(self.phy_num)
+        log2 = "{:02d}".format(self.log_num)
+        phyname = "phy"+phy2
+        logname = "fit"+phy2
         domain = 'faraday'
         return {
-            "name": nodename,
-            "hostname": nodename,
+            "name": phyname,
+            "hostname": logname,
             "hardware_type": "PC-Icarus",
-            "urn": "urn:publicid:IDN+omf:faraday+node+"+nodename,
+            "urn": "urn:publicid:IDN+omf:faraday+node+"+logname,
             "interfaces": [
                 {
-                    "name": nodename+":if0",
+                    "name": logname+":if0",
                     "role": "control",
                     "mac": self.mac,
                     "ip": {
-                        "address": "192.168.3.{}".format(self.number),
+                        "address": "192.168.3.{}".format(self.log_num),
                         "netmask": "255.255.255.0",
                         "ip_type": "ipv4"
                     }
                 },
                 {
-                    "name": nodename+":if1",
+                    "name": logname+":if1",
                     "role": "experimental",
                     "mac": self.alt_mac
                 }
             ],
             "cmc": {
-                "name": nodename+":cm",
-                "mac": "02:00:00:00:00:"+node2,
+                "name": logname+":cm",
+                "mac": "02:00:00:00:00:"+phy2,
                 "ip": {
-                    "address": "192.168.1.{}".format(self.number),
+                    "address": "192.168.1.{}".format(self.log_num),
                     "netmask": "255.255.255.0",
                     "ip_type": "ipv4"
                 }
@@ -58,29 +61,49 @@ import re
 import csv
 import json
 
+from argparse import ArgumentParser
 
 mac_regexp = re.compile ('(?P<prefix>([0-9A-Fa-f]{2}:){5})(?P<last>[0-9A-Fa-f]{2})')
 
-with open('fit.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile)
-    nodes = []
-    for lineno, line in enumerate(reader):
-        try:
-            number = int (line[1])
-            mac = line[4]
-            match = mac_regexp.match(mac)
-            if match:
-                prefix, last = match.group('prefix', 'last')
-                byte = int (last, base=16)
-                alt_last = str(byte-1)
-                alt_mac = prefix+alt_last
-                nodes.append(Node(number, mac, alt_mac))
-            else:
-                print lineno,'non-mac',mac
-        except Exception as e:
-            print 'skipping line',lineno,line
 
-    with open ('fit.json', 'w') as jsonfile:
-        models = [ node.model() for node in nodes ]
-        json.dump (models, jsonfile)
-        
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose")
+    parser.add_argument("-o", "--output", default=None)
+    parser.add_argument("input", type=str, default="fit.csv")
+    args = parser.parse_args()
+
+    output = args.output or args.input.replace(".csv","")+".json"
+    
+    with open(args.input, 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        nodes = []
+        for lineno, line in enumerate(reader):
+            try:
+                phy_num = int (line[1])
+                log_num = int (line[3])
+                mac = line[5]
+                match = mac_regexp.match(mac)
+                if match:
+                    prefix, last = match.group('prefix', 'last')
+                    byte = int (last, base=16)
+                    alt_last = str(byte-1)
+                    alt_mac = prefix+alt_last
+                    nodes.append(Node(phy_num, log_num, mac, alt_mac))
+                else:
+                    print lineno,'non-mac',mac
+            except Exception as e:
+                print 'skipping line',lineno,line
+                if args.verbose:
+                    import traceback
+                    traceback.print_exc()
+    
+        with open (output, 'w') as jsonfile:
+            models = [ node.model() for node in nodes ]
+            json.dump (models, jsonfile)
+
+    print ("(Over)wrote {args.output} from {args.input}".format(**locals()))
+    return 0
+
+if __name__ == '__main__':
+    exit(main())
