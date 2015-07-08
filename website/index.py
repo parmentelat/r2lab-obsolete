048eefa2401995cd414f2805ca8e2638f9b32027
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2015-07-08 20:02:52 +0200

    better index

--- website/index.py
+++ website/index.py
@@ -9,7 +9,16 @@ from glob import glob
 source = "index.md"
 output_dir = "markdown"
 
-index_line_format = " * [{base} ({dir})]({base}.html)\n"
+index_line_format = " * [{title}]({base}.html)\n"
+
+TITLE="title:"
+
+def get_title(filename):
+    with open(filename) as input:
+        for line in input:
+            if line.startswith(TITLE):
+                return line[len(TITLE):].strip()
+    return filename
 
 def main():
     if not os.path.isfile(source):
@@ -24,10 +33,11 @@ def main():
         for other in sys.argv[1:]:
             dir = os.path.dirname(other) or "."
             base = os.path.basename(other).replace(".md", "")
+            title = get_title(other)
             # skip index from the index...
             if base == "index":
                 continue
-            result.write(index_line_format.format(dir=dir, base=base))
+            result.write(index_line_format.format(**locals()))
     print("(Over)wrote {}".format(output))
 
 main()
