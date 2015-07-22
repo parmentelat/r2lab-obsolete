e3abb7f44d2b7c3fde65d5977211f27c78c969c2
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2015-07-22 14:43:57 +0200

    removed the python script - use sed instead add option for setting FEDERATED much nicer dry-run

--- inventory/create-certs.sh
+++ inventory/create-certs.sh
@@ -33,95 +33,50 @@ XMPP_DOMAIN=r2lab
 
 # the name for the root certificate
 ROOT=r2lab
-PRIVATE_AREA=/root/certificates-omf
+PRIVATE_AREA=/root/.omf
 PUBLIC_AREA=/etc/nitos_testbed_rc/trusted_roots
+# any file in .pem or .gid from this location will be rsynced under the public area
+FEDERATED=/root/certificates-federated/
 
 # create the root CA for 10 years
 CA_DURATION=$((3600*24*365*10))
 # others for 2 years
 DURATION=$((3600*24*365*2))
 
-# use option -f to force re-creation of exiting certificates
+# use option -n to run in dry-mode
+DRY_RUN=
+# use option -o to force overwriting of exiting certificates
 FORCE_OVERWRITE=
 
-#################### utility for splitting a pem into public and private parts
-# extract_key [-o private_key_filename] pem_filename
-function split-pem () {
-    python3 - "$@" << EOF
-#!/usr/bin/env python3
-
-import sys
-import os.path
-
-from argparse import ArgumentParser
-
-BEGIN = "-----BEGIN RSA PRIVATE KEY-----"
-
-def split_pem(cert_filename, pub_filename, priv_filename):
-    """
-    From
-    (*) cert_filename : expected to contain one full cert 
-                        (i.e. one with a private key) expected to be found
-    Creates 2 py-products
-    (*) pub_filename : that contains only the public part of said certificate
-    (*) priv_filename: that contains the public key
+#################### utility to display/run a command in dry-mode
+# show_and_run "some message" the command to be run
+# does not support piping or redirecting
+function show_and_run () {
+    message="$1"; shift
+    command="$@"
+    echo "$message using command:"
+    [ -n "$DRY_RUN" ] && echo -n "(dry)"
+    echo -e "\t $command"
+    # do it unless dry-run
+    [ -z "$DRY_RUN" ] && $command
+}    
     
-    Returns
-    Overwrite message when everything goes fine
-    or None if no private key is found in input
-    """
-    # load in memory
-    with open(cert_filename) as input:
-        full_cert_lines = input.readlines()
-    # if no private key is present, do nothing and return None
-    found = [ line for line in full_cert_lines if BEGIN in line ]
-    if not found:
-        return
-    # rewrite in 2 separate files
-    in_private = False
-    with open(pub_filename, 'w') as public:
-        with open(priv_filename, 'w') as private:
-            for line in full_cert_lines:
-                if BEGIN in line:
-                    in_private = True
-                    private.write(line)
-                else:
-                    output = public if not in_private else private
-                    output.write(line)
-    return [ "(Over)wrote {}\n".format(name)
-             for name in (pub_filename, priv_filename)]
+#################### utility for splitting a pem into public and private parts
+# split-pem full-cert public-part private-part
+function split-pem () {
+    full=$1; shift
+    public=$1; shift
+    private=$1; shift
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument("-u", "--public", dest='public', default=None,
-                        help="specify output file for public part")
-    parser.add_argument("-i", "--private", dest='private', default=None,
-                        help="specify output file for private part")
-    parser.add_argument('pem_certificate', help='input pem file')
-    args = parser.parse_args()
-    if not args.public:
-        args.public = args.pem_certificate.replace(".pem", ".public.pem")
-    if not args.private:
-        args.private = args.pem_certificate.replace(".pem", ".private.pem")
-    messages = split_pem(args.pem_certificate,
-                        args.public,
-                        args.private)
-    if not messages:
-        print("Nothing done (no private key in {})".format(args.pem_certificate))
-        return 1
-    else:
-        for line in messages:
-            print(line, end='')
-        return 0
-
-sys.exit(main())
-EOF
+    MARKER="-----BEGIN RSA PRIVATE KEY-----"
+    sed -n "/${MARKER}/q;p" $full > $public
+    (echo "${MARKER}" ; sed -n "/${MARKER}/"'{:a;n;p;ba}' $full) > $private
 }
 
 ### create a certificate if needed
 function create-certificate () {
     do_split=""
-    if [ "$1" == "-s" ] ; then
+    if [ "$1" == "--split" ] ; then
 	do_split=true
 	shift
     fi
@@ -142,22 +97,17 @@ function create-certificate () {
 	command="omf_cert.rb -o $certificate $@ --email $email"
 	[ "$resource_type" == "none" ] || command="$command --resource-type $resource_type"
 	command="$command --resource-id ${resource_id} --root ${PRIVATE_AREA}/${ROOT}.pem --duration $DURATION $verb"
-	echo "Creating $certificate using command:"
-	echo -e "\t $command"
-	# don't do it
-	[ -z "$DRY_RUN" ] && $command
+	show_and_run "Creating $certificate" $command
 	if [ -n "$do_split" ] ; then
-	    command="split-pem $certificate --public $public_only --private $private_key"
-	    echo "Extracting using command:"
-	    echo -e "\t $command"
-	    [ -z "$DRY_RUN" ] && $command
+	    command="split-pem $certificate $public_only $private_key"
+	    show_and_run "Extracting public/private into $public_only $private_key" $command
 	fi
     fi
 }
 
 function create-certificates () {
 
-    mkdir -p ${PRIVATE_AREA} ${PUBLIC_AREA} 
+    [ -z "$DRY_RUN" ] && mkdir -p ${PRIVATE_AREA} ${PUBLIC_AREA} 
 
     ## root certificate
     root_pem=${PRIVATE_AREA}/${ROOT}.pem
@@ -165,13 +115,10 @@ function create-certificates () {
 	echo "Preserving $root_pem"
     else
 	command="omf_cert.rb --email root@${DOMAIN} -o $root_pem --duration $CA_DURATION create_root"
-	echo "Creating root CA certificate $root_pem using command"
-	echo -e "\t $command"
-	[ -z "$DRY_RUN" ] && $command
-	command="split-pem $root_pem --public ${PUBLIC_AREA}/${ROOT}.public.pem"
-	echo "Extracting using command:"
-	echo -e "\t $command"
-	[ -z "$DRY_RUN" ] && $command
+	show_and_run "Creating root CA certificate $root_pem" $command
+	public_pem=${PUBLIC_AREA}/${ROOT}.public.pem
+	command="split-pem $root_pem $public_pem /dev/null"
+	show_and_run "Exposing public key into $public_pem" $command
     fi
     
 
@@ -181,42 +128,62 @@ function create-certificates () {
     create-certificate create_resource cm_factory cm_factory cm_factory
     create-certificate create_resource frisbee_factory frisbee_factory frisbee_factory
 
+    # these are the commands the doc says should be issued
     # omf_cert.rb -o user_factory.pem --email user_factory@${DOMAIN} --resource-type user_factory --resource-id xmpp://user_factory@${XMPP_DOMAIN} --root /root/.omf/trusted_roots/root.pem --duration 50000000 create_resource 
     # omf_cert.rb -o cm_factory.pem --email cm_factory@${DOMAIN} --resource-type cm_factory --resource-id xmpp://cm_factory@${XMPP_DOMAIN} --root /root/.omf/trusted_roots/root.pem --duration 50000000 create_resource
     # omf_cert.rb -o frisbee_factory.pem --email frisbee_factory@${DOMAIN} --resource-type frisbee_factory --resource-id xmpp://frisbee_factory@${XMPP_DOMAIN} --root /root/.omf/trusted_roots/root.pem --duration 50000000 create_resource
 
     ########## omf_sfa
 
-    create-certificate -s create_resource am am am_controller --geni_uri URI:urn:publicid:IDN+${AM_SERVER_DOMAIN}+user+am 
-    # omf_cert.rb -o am.pem  --geni_uri URI:urn:publicid:IDN+${AM_SERVER_DOMAIN}+user+am --email am@${DOMAIN} --resource-id xmpp://am_controller@${XMPP_DOMAIN} --resource-type am_controller --root ${REPO}/trusted_roots/root.pem --duration 50000000 create_resource
-    #extract_key_from_pem ${REPO}/am.pem -o ${REPO}/am.pkey
-    
+    create-certificate --split create_resource am am am_controller --geni_uri URI:urn:publicid:IDN+${AM_SERVER_DOMAIN}+user+am 
     create-certificate create_user user_cert root none --user root --geni_uri URI:urn:publicid:IDN+${AM_SERVER_DOMAIN}+user+root
+
+    # these are the commands the doc says should be issued
+    # omf_cert.rb -o am.pem  --geni_uri URI:urn:publicid:IDN+${AM_SERVER_DOMAIN}+user+am --email am@${DOMAIN} --resource-id xmpp://am_controller@${XMPP_DOMAIN} --resource-type am_controller --root ${REPO}/trusted_roots/root.pem --duration 50000000 create_resource
     # omf_cert.rb -o user_cert.pem --geni_uri URI:urn:publicid:IDN+${AM_SERVER_DOMAIN}+user+root --email root@${DOMAIN} --user root --root ${REPO}/trusted_roots/root.pem --duration 50000000 create_user
-    #extract_key_from_pem ${REPO}/user_cert.pem -o ${REPO}/user_cert.pkey
 }
 
-FEDERATED=/root/certificates-federated/
 
-function inria-specifics () {
-    set -x
-    for trusted in $FEDERATED/*.gid; do
-	rsync -av $trusted $PUBLIC_AREA/
+function publish-federated () {
+    # show message when federated not found
+    [ -d $FEDERATED ] || {
+	echo "Could not find $FEDERATED"
+	echo "You could store there .pem or .gid files that are meant to be exposed in $PUBLIC_AREA"
+	return
+    }
+    # restrict to .pem and .gid files only
+    echo "--------------------"
+    for trusted in $(ls -d $FEDERATED/*.pem $FEDERATED/*.gid 2> /dev/null); do
+	command="rsync -av $trusted $PUBLIC_AREA/"
+	show_and_run "Syncing federated trusted root $certificate" $command
+	echo "--------------------"
     done
     set +x
 }
 
+function restart-all () {
+    show_and_run "Restarting nitos testbed" restart ntrc
+    show_and_run "Restarting omf-sfa broker service" service omf-sfa restart
+}
+
 function main () {
-    while getopts "u:i:nf" opt; do
+    while getopts "u:i:f:on" opt; do
 	case $opt in
 	    u) PUBLIC_AREA=$OPTARG;;
 	    i) PRIVATE_AREA=$OPTARG;;
-	    f) FORCE_OVERWRITE=true;;
+	    f) FEDERATED=$OPTARG;;
+	    o) FORCE_OVERWRITE=true;;
 	    n) DRY_RUN=true;;
 	esac
     done
+    echo Will store private material in $PRIVATE_AREA
+    echo Will store public trusted roots in $PUBLIC_AREA
+    echo Will also mirror any .pem or .gid file from $FEDERATED with trusted roots
+    echo ====================
+
     create-certificates
-    [ -z "$DRY_RUN" ] && inria-specifics
+    publish-federated
+    restart-all
 }
 
 main "$@"
