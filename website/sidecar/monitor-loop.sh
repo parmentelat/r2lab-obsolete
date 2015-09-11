eb42be83bbd0a7b8ab14e4d1275faea7fefd75b2
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2015-09-11 09:50:56 +0200

    make monitor-loop log into /var/log not verbose by default anymore

--- website/sidecar/monitor-loop.sh
+++ website/sidecar/monitor-loop.sh
@@ -7,31 +7,36 @@ hostname | grep -q faraday || {
     }
 
 # details on where to put stuff
-remote=root@r2lab.pl.sophia.inria.fr
-remote_dest=/var/lib/sidecar/news.json
+REMOTE_ID=root@r2lab.pl.sophia.inria.fr
+REMOTE_DEST=/var/lib/sidecar/news.json
+LOCAL_LOG=/var/log/monitor.log
 
 # nodes can be specified on the command line, defaults to all nodes
 
 # options
 delay=5
+verbose=
 
 while getopts ":d:" opt; do
     case $opt in
 	d)
 	    delay=$OPTARG;;
+	v)
+	    verbose=-v ;;
     esac
 done
 shift $((OPTIND-1))
 
 cd /root/fitsophia/website/sidecar
 # just in case
-ssh $remote mkdir -p $(dirname $remote_dest)
+ssh $REMOTE_ID mkdir -p $(dirname $REMOTE_DEST)
 
 while true; do
-    echo ==================== Running script
-    ./monitor.py -v -o monitor.json "$@"
-    echo ==================== Installing result
+    echo ===== Running monitor.py
+    ./monitor.py $verbose -o monitor.json "$@"
+    echo ===== Pushing onto $REMOTE_ID:$REMOTE_DEST
     # a bit surprisingly, pushing with rsync won't do it, it feels like it uses tricks that bypass fs.watch
-    cat monitor.json | ssh $remote cat \> $remote_dest
+    cat monitor.json | ssh $REMOTE_ID cat \> $REMOTE_DEST
+    echo going to sleep for $delay s
     sleep $delay
-done
+done > $LOCAL_LOG 2>&1
