74653790de1ca566d4bee5b72d033e2bfc605c71
Mario Zancanaro <nano@creaator.com>
2015-10-09 16:12:15 +0200

    Functions comments... Grrrr

--- website/r2lab-layout/assets/js/randomImage.js
+++ website/r2lab-layout/assets/js/randomImage.js
@@ -1,6 +1,7 @@
 
 (function($) {
 
+	// returns a random images from a given list 
   getOneImage = function(imagesList, path) {
   	path = path || 'assets/img/';	
   	idx  = Math.round(Math.random() * (imagesList.length - 1));
@@ -8,6 +9,7 @@
   }
 
 
+  // set at background tag the image returned by getOneImage function
 	placeImage = function() {
 		var imagesList = ["back.jpg", "back_1.jpg", "back_2.jpg"];
 
