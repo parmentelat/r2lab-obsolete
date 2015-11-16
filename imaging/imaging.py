002bf9e81ecc0331b130a30bdcb0e3ea96d5ffda
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2015-11-16 13:43:57 +0100

    cleaner separation of selector-oriented features

--- imaging/imaging.py
+++ imaging/imaging.py
@@ -6,52 +6,15 @@ try:
 except:
     from asyncio import async as ensure_future
 
-import os
 from argparse import ArgumentParser
 
-from selector import Selector
+from selector import add_selector_arguments, selected_selector
 from cmc import CMC
-
-
-def add_range_arguments(parser):
-    parser.add_argument(
-        "-a", "--all-nodes", action='store_true', default=False,
-        help="""
-        add the contents of the ALL_NODES env. variable in the mix
-        this can be combined with ranges, like e.g.
-        -a ~4-16-2
-        """)
-    parser.add_argument(
-        "ranges", nargs="*",
-        help="""
-        nodes can be specified one by one, like 1 004 fit01 reboot01
-        by range (inclusive) like 2-12, fit4-reboot12,
-        by range+step like 4-16-2 which would be all even numbers from 4 to 16,
-        ranges can also be excluded with ~; so ~1-4 means remove 1,2,3,4
-        """)
-        
-def selected_selector(parser_args):        
-    ranges = parser_args.ranges
-    
-    # our naming conventions
-    selector = Selector('fit', 'reboot')
-    # nothing set on the command line : let's use $NODES
-    if parser_args.all_nodes:
-        selector.add_range(os.environ["ALL_NODES"])
-    if not ranges:
-        for node in os.environ["NODES"].split():
-            selector.add_range(node)
-    else:
-        for range in ranges:
-            selector.add_range(range)
-
-    return selector
-
 
 def load():
     parser = ArgumentParser()
     parser.add_argument("-v", "--verbose", action='store_true', default=False)
-    add_range_arguments(parser)
+    add_selector_arguments(parser)
     args = parser.parse_args()
 
     selector = selected_selector(args)
--- imaging/selector.py
+++ imaging/selector.py
@@ -1,5 +1,7 @@
 #/usr/bin/env python3
 
+import os
+
 class Selector:
 
     # typically regularname='fit' and rebootname='reboot'
@@ -52,3 +54,42 @@ class Selector:
         return ("{}{:02}".format(self.regularname, i) for i in self.set)
     def cmc_names(self):
         return ("{}{:02}".format(self.rebootname, i) for i in self.set)
+
+
+####################
+#convenience tools shared by all commands that need this sort of selection
+# maybe we should have specialized ArgumentParser instead
+def add_selector_arguments(arg_parser):
+    arg_parser.add_argument(
+        "-a", "--all-nodes", action='store_true', default=False,
+        help="""
+        add the contents of the ALL_NODES env. variable in the mix
+        this can be combined with ranges, like e.g.
+        -a ~4-16-2
+        """)
+    arg_parser.add_argument(
+        "ranges", nargs="*",
+        help="""
+        nodes can be specified one by one, like 1 004 fit01 reboot01
+        by range (inclusive) like 2-12, fit4-reboot12,
+        by range+step like 4-16-2 which would be all even numbers from 4 to 16,
+        ranges can also be excluded with ~; so ~1-4 means remove 1,2,3,4
+        """)
+        
+# parser_args is the result of arg_parser.parse_args()
+def selected_selector(parser_args):        
+    ranges = parser_args.ranges
+    
+    # our naming conventions
+    selector = Selector('fit', 'reboot')
+    # nothing set on the command line : let's use $NODES
+    if parser_args.all_nodes:
+        selector.add_range(os.environ["ALL_NODES"])
+    if not ranges:
+        for node in os.environ["NODES"].split():
+            selector.add_range(node)
+    else:
+        for range in ranges:
+            selector.add_range(range)
+
+    return selector
