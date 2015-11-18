c37a1ddc7d033314f581b577c2c49f53d684e604
Mario Zancanaro <nano@creaator.com>
2015-11-18 17:02:30 +0100

    Some selected nodes... another test

--- inventory/nightly-routine.sh
+++ inventory/nightly-routine.sh
@@ -1,4 +1,4 @@
 SHELL=/bin/sh
 HOME=/root
 PATH=/root/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
-python /root/fitsophia/nightly/reset_faraday.py -N [4-37] > /var/log/reset_faraday.log 2>&1;
+python /root/fitsophia/nightly/reset_faraday.py -N 2,4,17,18,19,20,22,24,25,26,27,28,29 > /var/log/reset_faraday.log 2>&1;
