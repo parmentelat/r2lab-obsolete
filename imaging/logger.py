833d2174112fbf63041616f21eeb36b209d7472c
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2015-11-20 15:24:41 +0100

    logger

---
+++ imaging/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+log_format = '%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s'
+
+# hard-wire for now
+logging.basicConfig(
+    level=logging.INFO,
+    format = log_format,
+    filename = 'imaging.log'
+    )
+
+logger = logging.getLogger('imaging')
