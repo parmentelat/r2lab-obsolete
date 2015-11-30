0318fd1479c84a35d9f99dfbfd3958863a9cad79
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2015-11-30 15:08:51 +0100

    script for pulling images from preplab to faraday

---
+++ rhubarbe-images/pull-from-preplab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+this=$(basename $0)
+
+repo=/var/lib/rhubarbe-images
+preplab=bemol.pl.sophia.inria.fr
+
+hostname | grep -q faraday || { echo "Must run on faraday"; exit 1; }
+
+rsync "$@" -av --delete --exclude $this --exclude archive --exclude \*.log --exclude root-node\* $preplab:$repo/ $repo/ 
+
