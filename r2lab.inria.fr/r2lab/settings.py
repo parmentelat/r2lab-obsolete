6619f542478478167ea435a8907bd88ff5c1b081
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-01-20 16:00:37 +0100

    another tweak for wsgi/prod mode

--- r2lab.inria.fr/r2lab/settings.py
+++ r2lab.inria.fr/r2lab/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'r2lab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ 'templates' ],
+        'DIRS': [ BASE_DIR + '/templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
