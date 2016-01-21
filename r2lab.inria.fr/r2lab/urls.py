7c947cf166c9ae810788a9d1292bf895534271d4
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-01-21 12:26:09 +0100

    redirect to /index

--- r2lab.inria.fr/r2lab/urls.py
+++ r2lab.inria.fr/r2lab/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    url(r'^(/)?$', RedirectView.as_view(url='/index', permanent=False), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^md/', include('md.urls')),
 ] \
