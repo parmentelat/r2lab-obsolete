79641d60d10b399296723cc88f52fd7afa508ec1
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-01-29 14:41:24 +0100

    new template tag 'jsonify', used to expose globals to javascript named r2lab_user and r2lab_slices

---
+++ r2lab.inria.fr/templates/r2lab/r2lab_user.js
@@ -0,0 +1,6 @@
+{% load jsonify %}
+<script type="text/javascript">
+/* globals that describe logged in user */
+r2lab_user = {{r2lab_context.mfuser|jsonify|safe}};
+r2lab_slices = {{r2lab_context.slices|jsonify|safe}};
+</script>
