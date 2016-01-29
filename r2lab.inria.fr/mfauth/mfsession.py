3c32c0eaf41446ab9e929ca8e94577c4d2a646c6
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-01-29 10:56:43 +0100

    "retrieve user's slices at manifold now available as r2lab_context['slices']" merged from django

--- r2lab.inria.fr/mfauth/mfsession.py
+++ r2lab.inria.fr/mfauth/mfsession.py
@@ -11,12 +11,21 @@ from manifoldapi.manifoldapi import ManifoldAPI
 
 def get_details(url, email, password, logger):
     """
-    return a tuple with (session, person, api)
+    return a tuple with (session, auth, person, slices)
     associated with these credentials at the manifold backend
     
+    * session : the session with the manifold API
+    * auth : ready-to use auth to talk to the API again
+    * person : the manifold object as-is - looks like this
+	{'status': 2, 'config': '{"firstname": "Thierry", 
+                                  "lastname": "Parmentelat", 
+                                  "authority": "onelab.inria"}',
+         'user_id': 55, 'email': 'thierry.parmentelat@inria.fr', 'password': '<>'}
+    * slices : the list of slice hrns that the person is in
+
     in case of failure, return tuple with same size with only None's
     """
-    failure_result = None, None, None
+    failure_result = None, None, None, None
     
     auth = {'AuthMethod': 'password', 'Username': email, 'AuthString': password}
     api = ManifoldAPI(url, auth)
@@ -38,27 +47,64 @@ def get_details(url, email, password, logger):
         return failure_result
     person = persons[0]
     logger.debug("PERSON : {}".format(person))
-    return session, person, api
+
+    # get related slices
+    query = Query.get('myslice:user').filter_by('user_hrn', '==', '$user_hrn').select(['user_hrn', 'slices'])
+    mf_result = api.forward(query.to_dict())
+    try:
+        # xxx use ManifoldResult ?
+        # this is a list of dicts that have s 'slices' field
+        nm_d_s = mf_result['value']
+        slices = [ nm for nm_d in nm_d_s for nm in nm_d['slices'] ]
+    except Exception as e:
+        import traceback
+        logger.error("mfsession: Could not retrieve user's slices\n{}"
+                     .format(traceback.format_exc()))
+        slices = []
+
+    return session, auth, person, slices
 
 ####################
 if __name__ == '__main__':
 
     test_manifold_url = "https://portal.onelab.eu:7080/"
 
-    tests = [
+    default_tests = [
         # should succeed
         ( 'demo', 'demo'),
         # should fail
         ( 'demo', 'not-the-password'),
     ]
 
+    # with an empty argv, we use the tests above
+    # but you can also specify mail and password on the command line
+    import sys
+    args = sys.argv[1:]
+    if not args:
+        tests = default_tests
+    elif len(args) % 2 != 0:
+        print("need an even number of args (email / passwd)")
+        exit(1)
+    else:
+        def chunks(l, n):
+            """Yield successive n-sized chunks from l."""
+            for i in range(0, len(l), n):
+                yield l[i:i+n]
+        tests = chunks(args, 2)
+    
+
     import logging
     logger = logging.getLogger('session')
     for email, password in tests:
-        session, person, api = get_details(test_manifold_url, email, password, logger)
-        if session:
-            print("get_session: OK with {} / {} : returned\nsession\t{}\nperson\t{}\napi\t{}\n".
-                  format(email, password, session, person, api))
-            api_ok = api
+        print(10*'=', "testing {} / {}".format(email, password))
+        import time
+        beg = time.time()
+        tuple = get_details(test_manifold_url, email, password, logger)
+        end = time.time()
+        if tuple[0]:
+            print("get_details: OK")
+            for elem in tuple:
+                print("\t{}".format(elem))
         else:
-            print("get_session: NOPE with {} / {} : authenticated KO".format(email, password))
+            print("NOPE - get_details authenticated KO".format(email, password))
+        print("total duration for get_details = {}s".format(end-beg))
