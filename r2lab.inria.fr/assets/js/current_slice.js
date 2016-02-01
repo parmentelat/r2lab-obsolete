35c4e0c4f053dbe7a7b74efbac07d3a8002265d4
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-02-01 16:43:05 +0100

    current_slice more strongly encapsulated now a single global named 'current_slice' with the slicename stored in current_slice.name

--- r2lab.inria.fr/assets/js/current_slice.js
+++ r2lab.inria.fr/assets/js/current_slice.js
@@ -2,63 +2,83 @@
  * tools for managing current slice
  */
 
-// xxx todo - group all this inside a 'current_slice' global variable
+var current_slice = {
+    // the value of interest
+    name : "",
 
-var current_slice_key = 'r2lab:current_slice';
-// for styling selected slice
-var current_slice_class = 'current_slice';
+    // the key for storing the value
+    key : 'r2lab:current_slice',
+    // the class for decorating html elements
+    klass : 'current_slice',
 
-// expose this function to r2lab_user.js for retrieving current slice upon page load
-// avail_slices is the full list as retrieved at the registry
-function current_slice_get_last(avail_slices) {
-    if (avail_slices.length == 0) {
-	console.log("WARNING: current_slice_get_last: no slices to chose from");
-	return "";
-    } else {
-	default_slice = avail_slices[0];
-    }
-    // non-html5 browsers
-    if (typeof(Storage) === "undefined") {
-	console.log("WARNING: current_slice_get_last: no local storage");
-	return default_slice;
-    } 
-    stored = localStorage.getItem(current_slice_key);
-    if (avail_slices.indexOf(stored) >= 0) {
-	return stored;
-    }
-    current_slice_store(default_slice);
-    return default_slice;
-}
+    // retrieve current slice upon page load
+    // avail_slices is the full list as retrieved at the registry
+    // mostly for the r2lab_user.js template
+    init_from_storage : function(avail_slices) {
+	current_slice.name = current_slice.get(avail_slices);
+	current_slice.store(current_slice.name);
+	console.log("restored -> " + current_slice.name);
+    },
 
-function current_slice_store(slice) {
-    if (typeof(Storage) === "undefined") {
-	console.log("WARNING: current_slice_store : no local storage");
-    } else {
-	localStorage.setItem(current_slice_key, slice);
-    }
-}
-
-// highlight the item corresponding to current slice
-function current_slice_update_selectors() {
-    $('.set_current_slice').each(function(index){
-        if ($(this).attr('slicename') == r2lab_current_slice){
-	    $(this).addClass(current_slice_class);
-	} else {
-	    $(this).removeClass(current_slice_class);
+    get : function(avail_slices) {
+	if (avail_slices.length == 0) {
+	    console.log("WARNING: current_slice.get_last: no slices to chose from");
+	    return "";
 	}
-    });
-}
-// all elements of type 'set_current_slice' will have a click function
-// defined on them that will change the current slice name
-(function($){
-    var arm_selectors = function() {
+	var default_slice = avail_slices[0];
+	// non-html5 browsers
+	if (typeof(Storage) === "undefined") {
+	    console.log("WARNING: current_slice.get_last: no local storage");
+	    return default_slice;
+	} 
+	stored = localStorage.getItem(current_slice.key);
+	// check it's still current
+	if (avail_slices.indexOf(stored) >= 0) {
+	    return stored;
+	}
+	return default_slice;
+    },
+
+    store : function(slice) {
+	console.log('storing ' + slice);
+	if (typeof(Storage) === "undefined") {
+	    console.log("WARNING: current_slice.store : no local storage");
+	} else {
+	    localStorage.setItem(current_slice.key, slice);
+	}
+    },
+
+    // highlight the item corresponding to current slice
+    update_selectors : function() {
+	console.log("updating selectors : " + current_slice.name);
+	$('.set_current_slice').each(function(index){
+            if ($(this).attr('slicename') == current_slice.name){
+		$(this).addClass(current_slice.klass);
+	    } else {
+	    $(this).removeClass(current_slice.klass);
+	    }
+	});
+    },
+				     
+    // all elements of type 'set_current_slice' need a click function
+    // defined on them that will change the current slice name
+    arm_selectors : function() {
 	$('.set_current_slice').on("click", function(){
 	    var new_slice = $(this).attr("slicename");
-	    current_slice_store(new_slice);
-	    r2lab_current_slice = new_slice;
-	    current_slice_update_selectors();
+	    console.log('new slice ' + new_slice);
+	    current_slice.store(new_slice);
+	    current_slice.name = new_slice;
+	    current_slice.update_selectors();
 	});
-    };
-    $(arm_selectors);
-    $(current_slice_update_selectors);
-})(jQuery);
+    },
+
+    // stuff to call on page load
+    init : function(){
+	console.log("0.1 " + current_slice.name);
+	current_slice.update_selectors();
+	current_slice.arm_selectors();
+	console.log("0.2 " + current_slice.name);
+	console.log("0.3 " + current_slice.name);
+    }
+};
+$(current_slice.init)
--- r2lab.inria.fr/templates/r2lab/r2lab_user.js
+++ r2lab.inria.fr/templates/r2lab/r2lab_user.js
@@ -1,8 +1,14 @@
+{% load staticfiles %}
 {% load jsonify %}
+<script type="text/javascript" src="{% static 'js/current_slice.js' %}"></script>
 <script type="text/javascript">
 /* globals that describe logged in user */
 r2lab_user = {{r2lab_context.mfuser|jsonify|safe}};
 r2lab_hrn = {{r2lab_context.mfuser.hrn|jsonify|safe}};
 r2lab_slices = {{r2lab_context.slices|jsonify|safe}};
-r2lab_current_slice = current_slice_get_last(r2lab_slices);
+// initialize current_slice - set of slices is needed there
+console.log("before -> "+ current_slice.name);
+current_slice.init_from_storage(r2lab_slices);
+console.log("after -> "+ current_slice.name);
+// javascript code should use  'current_slice.name'
 </script>
