f35ef46fdf6b11abb8669de74c691d15ecc5e371
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-02-01 15:23:59 +0100

    manage, visualize and expose r2lab_current_slice value is stored in browser

--- r2lab.inria.fr/assets/js/current_slice.js
+++ r2lab.inria.fr/assets/js/current_slice.js
@@ -1,10 +1,64 @@
+/*
+ * tools for managing current slice
+ */
+
+// xxx todo - group all this inside a 'current_slice' global variable
+
+var current_slice_key = 'r2lab:current_slice';
+// for styling selected slice
+var current_slice_class = 'current_slice';
+
+// expose this function to r2lab_user.js for retrieving current slice upon page load
+// avail_slices is the full list as retrieved at the registry
+function current_slice_get_last(avail_slices) {
+    if (avail_slices.length == 0) {
+	console.log("WARNING: current_slice_get_last: no slices to chose from");
+	return "";
+    } else {
+	default_slice = avail_slices[0];
+    }
+    // non-html5 browsers
+    if (typeof(Storage) === "undefined") {
+	console.log("WARNING: current_slice_get_last: no local storage");
+	return default_slice;
+    } 
+    stored = localStorage.getItem(current_slice_key);
+    if (avail_slices.indexOf(stored) >= 0) {
+	return stored;
+    }
+    current_slice_store(default_slice);
+    return default_slice;
+}
+
+function current_slice_store(slice) {
+    if (typeof(Storage) === "undefined") {
+	console.log("WARNING: current_slice_store : no local storage");
+    } else {
+	localStorage.setItem(current_slice_key, slice);
+    }
+}
+
+// highlight the item corresponding to current slice
+function current_slice_update_selectors() {
+    $('.set_current_slice').each(function(index){
+        if ($(this).attr('slicename') == r2lab_current_slice){
+	    $(this).addClass(current_slice_class);
+	} else {
+	    $(this).removeClass(current_slice_class);
+	}
+    });
+}
 // all elements of type 'set_current_slice' will have a click function
 // defined on them that will change the current slice name
 (function($){
-  var init = function() {
-    $('.set_current_slice').on("click", function(){
-       console.log("current_slice should now become: " + $(this).attr("slicename"));
-    });
+    var arm_selectors = function() {
+	$('.set_current_slice').on("click", function(){
+	    var new_slice = $(this).attr("slicename");
+	    current_slice_store(new_slice);
+	    r2lab_current_slice = new_slice;
+	    current_slice_update_selectors();
+	});
     };
-    $(init);
+    $(arm_selectors);
+    $(current_slice_update_selectors);
 })(jQuery);
--- r2lab.inria.fr/templates/r2lab/r2lab_user.js
+++ r2lab.inria.fr/templates/r2lab/r2lab_user.js
@@ -4,4 +4,5 @@
 r2lab_user = {{r2lab_context.mfuser|jsonify|safe}};
 r2lab_hrn = {{r2lab_context.mfuser.hrn|jsonify|safe}};
 r2lab_slices = {{r2lab_context.slices|jsonify|safe}};
+r2lab_current_slice = current_slice_get_last(r2lab_slices);
 </script>
