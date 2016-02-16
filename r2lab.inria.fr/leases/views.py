9b89187dfbd23e25b3b9e6958c8e8658272b8e7f
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-02-16 17:38:12 +0100

    leases-management URLs are now operational

--- r2lab.inria.fr/leases/views.py
+++ r2lab.inria.fr/leases/views.py
@@ -46,7 +46,7 @@ class LeasesProxy(View):
                 {'error' : "Unknown verb {}".format(verb)})
 
     # JSON encode and wrap into a HttpResponse
-    def http_response_from_struct(answer):
+    def http_response_from_struct(self, answer):
         return HttpResponse(json.dumps(answer))
 
     # xxx all the hard-wired data here needs to become configurable
@@ -55,31 +55,65 @@ class LeasesProxy(View):
     # we need to create a loop object for each hit on this URL
     # asyncio does create a loop object (get_event_loop())
     # but this is attached to main thread, it's not usable in this context
+    # xxx we might need to do some cleanup on loops at some point 
     def init_omf_sfa_proxy(self):
         self.loop = asyncio.new_event_loop()
-        # xxx we might need to do some cleanup on loops at some point 
+        # we use this location on r2lab.inria.fr which is readable by apache
         self.omf_sfa_proxy \
             = OmfSfaProxy("faraday.inria.fr", 12346,
-                          "/root/.omf/user_cert.pem", None,
+                          "/etc/rhubarbe/root.pem", None,
                           "37nodes",
                           loop=self.loop)
 
-    def missing_message(self, record, keys):
-        missing = [ k for k in keys if k not in record ]
+    def check_record(self, record, mandatory, optional):
+        """
+        checks for the keys in the input record
+        
+        returns None if everything is OK
+        and an error message otherwise
+        """
+        missing = { k for k in mandatory if k not in record }
+        known = set(mandatory) | set(optional)
+        print("known = ", known)
+        unknown = { k for k in record if k not in known }
+        error = ""
         if missing:
-            return {'error': "missing mandatory field(s)".format(" ".join(missing))}
+            error += "missing mandatory field(s): {}".format(" ".join(missing))
+        if unknown:
+            error += " unknown field(s): {}".format(" ".join(unknown))
+        return None if not error else {'error' : error}
 
+    def return_lease(self, lease):
+        """
+        what to return upon ADD or UPDATE
+        """
+        return {'uuid' : lease['uuid'],
+                'slicename' : lease['account']['name'],
+                'valid_from' : lease['valid_from'],
+                'valid_until' : lease['valid_until'],
+                'ok' : OmfSfaProxy.is_accepted_lease(lease),
+            }
+
+    #################### ADD
     def add_lease(self, record):
-        error = self.missing_message(
-            record,
-            ['slicename', 'valid_from', 'valid_until'])
+        error = self.check_record(record,
+                                  ('slicename', 'valid_from', 'valid_until'), ())
         if error:
             return self.http_response_from_struct(error)
         self.init_omf_sfa_proxy()
-        result = self.loop.run_until_complete(self.co_add_lease(record))
-        print ("add_lease complete, result = {}",format(result))
-#        del loop
-        return self.http_response_from_struct(result)
+        js = self.loop.run_until_complete(self.co_add_lease(record))
+        try:
+            result = json.loads(js)
+            lease = result['resource_response']['resource']
+            return self.http_response_from_struct(
+                self.return_lease(lease))
+        except:
+            import traceback
+            traceback.print_exc()
+            return self.http_response_from_struct(
+                {'error': 'unexpected error',
+                 'raw_result' : js,
+             })
 
     @asyncio.coroutine
     def co_add_lease(self, record):
@@ -94,3 +128,58 @@ class LeasesProxy(View):
         result = yield from self.omf_sfa_proxy.REST_as_json("leases", "POST", lease_request)
         return result
 
+    #################### UPDATE
+    def update_lease(self, record):
+        error = self.check_record(record,
+                                  ('uuid',), ('valid_from', 'valid_until'))
+        if error:
+            return self.http_response_from_struct(error)
+        self.init_omf_sfa_proxy()
+        js = self.loop.run_until_complete(self.co_update_lease(record))
+        try:
+            result = json.loads(js)
+            lease = result['resource_response']['resource']
+            return self.http_response_from_struct(
+                self.return_lease(lease))
+        except:
+            import traceback
+            traceback.print_exc()
+            return self.http_response_from_struct(
+                {'error': 'unexpected error',
+                 'raw_result' : js,
+             })
+
+    @asyncio.coroutine
+    def co_update_lease(self, record):
+        lease_request = record
+        result = yield from self.omf_sfa_proxy.REST_as_json("leases", "PUT", lease_request)
+        return result
+
+        
+    #################### DELETE
+    def delete_lease(self, record):
+        error = self.check_record(record, ('uuid',), ())
+        if error:
+            return self.http_response_from_struct(error)
+        self.init_omf_sfa_proxy()
+        js = self.loop.run_until_complete(self.co_delete_lease(record))
+        try:
+            result = json.loads(js)
+            ok = result['resource_response']['response']
+            return self.http_response_from_struct(
+                {'ok' : ok=="OK"})
+        except:
+            import traceback
+            traceback.print_exc()
+            return self.http_response_from_struct(
+                {'error': 'unexpected error',
+                 'raw_result' : js,
+             })
+
+    @asyncio.coroutine
+    def co_delete_lease(self, record):
+        lease_request = record
+        result = yield from self.omf_sfa_proxy.REST_as_json("leases", "DELETE", lease_request)
+        return result
+
+        
--- r2lab.inria.fr/markdown/ajax-leases.md
+++ r2lab.inria.fr/markdown/ajax-leases.md
@@ -1,29 +1,89 @@
 ---
-This is a temporary page to demonstrate how to post lease-action requests back to the django server
+This is a temporary page to demonstrate and test how to post lease-action requests back to the django server
 
----
-<div id="ajax-lease"><p>Click this paragraph to POST a fake JSON request to the /leases/add/ URL</p>
-<p id='response'>Result will show up here (see <code>leases/view.py</code>)</p>
-</div>
+This is a unit test; all data is hard-coded in the page itself
+
+See also `leases/view.py`
 
 <!-- this exposes the getCookie function -->
 <script type="text/javascript" src="/plugins/ajax-leases.js"></script>
 
+---
+<div id="add-lease"><p>Click this paragraph to add a lease</p>
+<p id='add-response'>Result here</p>
+</div>
+
 <script>
-// an example of how to create a lease
-var ajax_lease = function() {
+var added_lease_uuid;
+// an example of how to add a lease
+var add_lease = function() {
     var request = { 
     		    "slicename" : 'onelab.inria.mario.tutorial',
-                    "valid_from": "2016-02-16T22:30:00Z",
-                    "valid_until": "2016-02-16T23:00:00Z"
+                    "valid_from": "2016-02-20T08:00:00Z",
+                    "valid_until": "2016-02-20T09:00:00Z"
 		    };
     post_lease_request('add', request, function(xhttp) {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
-          document.getElementById("response").innerHTML = xhttp.responseText;
+          document.getElementById("add-response").innerHTML = xhttp.responseText;
 	  // decoding
 	  var answer = JSON.parse(xhttp.responseText);
-	  console.log("message = " + answer['message']);
+	  console.log(answer);
+	  added_lease_uuid = answer['uuid'];
+	  console.log("stored uuid = " + added_lease_uuid);
       }});
 }
-$(function(){$('#ajax-lease').click(ajax_lease);})
+$(function(){$('#add-lease').click(add_lease);})
 </script>
+
+
+
+---
+<div id="update-lease"><p>Click this paragraph to update the just-added lease</p>
+<p id='update-response'>Result here</p>
+</div>
+
+<script>
+// an example of how to update a lease
+var update_lease = function() {
+    var request = { 
+    		    "uuid" : added_lease_uuid,
+                    "valid_from": "2016-02-20T11:00:00Z",
+                    "valid_until": "2016-02-20T12:00:00Z"
+		    };
+    post_lease_request('update', request, function(xhttp) {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+          document.getElementById("update-response").innerHTML = xhttp.responseText;
+	  // decoding
+	  var answer = JSON.parse(xhttp.responseText);
+	  console.log(answer);
+      }});
+}
+$(function(){$('#update-lease').click(update_lease);})
+</script>
+
+
+
+---
+<div id="delete-lease"><p>Click this paragraph to delete the just-added lease</p>
+<p id='delete-response'>Result here</p>
+</div>
+
+<script>
+// an example of how to delete a lease
+var delete_lease = function() {
+    var request = { 
+    		    "uuid" : added_lease_uuid,
+		    };
+    post_lease_request('delete', request, function(xhttp) {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+          document.getElementById("delete-response").innerHTML = xhttp.responseText;
+	  // decoding
+	  var answer = JSON.parse(xhttp.responseText);
+	  console.log(answer);
+      }});
+}
+$(function(){$('#delete-lease').click(delete_lease);})
+</script>
+
+
+
