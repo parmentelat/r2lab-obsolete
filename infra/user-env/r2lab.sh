71d01774e3bc81480c0438e2d4a648eba15ea4df
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-02-26 10:22:23 +0100

    primary is logs-django, logs-r2lab is the alias

--- infra/user-env/r2lab.sh
+++ infra/user-env/r2lab.sh
@@ -1,5 +1,5 @@
 # not sure where this log file is going to end up
-alias logs-r2lab="tail -f /var/lib/r2lab.inria.fr/django.log /var/log/httpd/*log"
-alias logs-django=logs-r2lab
+alias logs-django="tail -f /var/lib/r2lab.inria.fr/django.log /var/log/httpd/*log"
+alias logs-r2lab=logs-django
 
 alias logs-sidecar="tail -f /var/log/sidecar.log"
