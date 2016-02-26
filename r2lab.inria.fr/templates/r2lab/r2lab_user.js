5d7dcb3f349e44ccff54ff753421b79596ea9787
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-02-26 16:28:39 +0100

    reorganize the assets/ area to isolate * what comes from fit/onelab -> assets/fit * what is our own stuff in assets/r2lab this last thing could arguably host the plugins as well...

--- r2lab.inria.fr/templates/r2lab/r2lab_user.js
+++ r2lab.inria.fr/templates/r2lab/r2lab_user.js
@@ -1,5 +1,5 @@
 {% load jsonify %}
-<script type="text/javascript" src="/assets/js/current_slice.js"></script>
+<script type="text/javascript" src="/assets/r2lab/current-slice.js"></script>
 <script type="text/javascript">
 // globals that describe logged in user 
 r2lab_user = {{r2lab_context.mfuser|jsonify|safe}};
