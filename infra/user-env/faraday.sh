226c539388e234332225d6e0024b0468fa2cff14
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-03-02 16:27:45 +0100

    27 back to its nominal location in the chamber

--- infra/user-env/faraday.sh
+++ infra/user-env/faraday.sh
@@ -225,7 +225,7 @@ function nodes-restore () {
 doc-alt nodes-restore "use previously named selection"
 
 #bemol && export _all_nodes=38-42 || export _all_nodes=1-37
-bemol && export _all_nodes="27 38-41" || export _all_nodes=1-37
+bemol && export _all_nodes=38-42 || export _all_nodes=1-37
 
 function all-nodes () { nodes $_all_nodes; }
 function nodes-all () { all-nodes; }
