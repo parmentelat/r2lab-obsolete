9b1cac4ca769cb49aa5f0ef2a964caeb09994164
Mario Zancanaro <nano@creaator.com>
2016-03-17 17:00:01 +0100

    Fixing image for background

--- r2lab.inria.fr/assets/r2lab/random-image.js
+++ r2lab.inria.fr/assets/r2lab/random-image.js
@@ -1,7 +1,7 @@
 
 (function($) {
 
-	// returns a random image from a given image list already placed in assets/img folder 
+	// returns a random image from a given image list already placed in assets/img folder
   getOneImage = function(imagesList, path) {
       path = path || '/assets/img/';
   	idx  = Math.round(Math.random() * (imagesList.length - 1));
@@ -11,9 +11,10 @@
 
   // set at background tag the image returned by getOneImage function
 	placeImage = function() {
-		var imagesList = ["back.jpg", "back_1.jpg", "back_2.jpg"];
+		// var imagesList = ["back.jpg", "back_1.jpg", "back_2.jpg"];
+    var imagesList = ["back_2.jpg"];
 
-  	$('#background').css("background-image", "url("+getOneImage(imagesList)+")"); 
+  	$('#background').css("background-image", "url("+getOneImage(imagesList)+")");
 	}
 
 })(jQuery);
