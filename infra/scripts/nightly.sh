a59b7eedac626228d0acf5d02f18c98fbf8bd7f6
Mario Zancanaro <nano@creaator.com>
2016-05-13 22:05:28 +0200

    Removing 11 and 23

--- infra/scripts/nightly.sh
+++ infra/scripts/nightly.sh
@@ -1,4 +1,4 @@
 SHELL=/bin/sh
 HOME=/root
 PATH=/root/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
-python /root/r2lab/nightly/nightly_shift.py -N all -a 11,23 > /var/log/nightly_shift.log 2>&1;
+python /root/r2lab/nightly/nightly_shift.py -N all > /var/log/nightly_shift.log 2>&1;
