789520ef84e7289f84bfd2844d1df5ddfef197f4
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-05-19 15:11:02 +0200

    nominal96 looks good

--- infra/user-env/oai-enb.sh
+++ infra/user-env/oai-enb.sh
@@ -165,6 +165,7 @@ s,mobile_network_code =.*,mobile_network_code = "95";,
 s,192.168.12.170,192.168.${oai_subnet}.$gw_id,
 s,eth4,${oai_ifname},
 s,192.168.12.242/24,192.168.${oai_subnet}.$id/24,g
+s,pucch_p0_Nominal.*,pucch_p0_Nominal = -96;,
 EOF
 # s,tx_gain.*,tx_gain = 80;,
 # s,rx_gain.*,rx_gain = 80;,
