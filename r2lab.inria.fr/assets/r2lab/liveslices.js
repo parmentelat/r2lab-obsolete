6e5f9fa5fc50e26d0a0f6aa2b4b249af89c24280
Mario Zancanaro <nano@creaator.com>
2016-05-30 11:50:06 +0200

    Bug fix for slices with 500 error

--- r2lab.inria.fr/assets/r2lab/liveslices.js
+++ r2lab.inria.fr/assets/r2lab/liveslices.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
     post_omfrest_request('/slices/get', request, function(xhttp) {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
 
-      var responses = JSON.parse(xhttp.responseText);
+        var responses = JSON.parse(xhttp.responseText);
     	  $(body).html("<div class='row'>\
                         <div class='col-md-6'><b>name</b></div>\
                         <div class='col-md-4'><b>expiration date</b></div>\
@@ -59,7 +59,6 @@ $(document).ready(function() {
 
           var s_class   = 'in_green';
           var s_message = 'valid';
-          var which_one = ['', ''];
           var s_icon    = '';
           var the_date  = moment(expiration).format("YYYY-MM-DD HH:mm");
           if (isPastDate(expiration) || isPastDate(closed)){
@@ -87,17 +86,88 @@ $(document).ready(function() {
                           </div>");
           $('a').tooltip();
         }
-     }
-   });
+      }
+    });
   }
 
+
+  var try_slices = function(id, names) {
+    var body = "#"+id;
+    $(body).html("<div class='row'>\
+                    <div class='col-md-6'><b>name</b></div>\
+                    <div class='col-md-4'><b>expiration date</b></div>\
+                    <div class='col-md-2'>&nbsp;</div>\
+                  </div>");
+    $.each( names, function( index, value ){
+
+      var request = {};
+      request['names'] = [value];
+
+      post_omfrest_request('/slices/get', request, function(xhttp) {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+          var responses = JSON.parse(xhttp.responseText);
+
+      	  for (i = 0; i < responses.length; i++) {
+            var response   = responses[i];
+            var slicename  = response['name'];
+            var expiration = response['valid_until'];
+            var closed     = response['closed_at'];
+            //var expiration = '2016-01-22T09:25:31Z';
+
+            var s_class   = 'in_green';
+            var s_message = 'valid';
+            var s_icon    = '';
+            var the_date  = moment(expiration).format("YYYY-MM-DD HH:mm");
+            if (isPastDate(expiration) || isPastDate(closed)){
+              sendMessage('One or more of your slices had expired. Click \
+                          <a href="#" data-toggle="modal" data-target="#slice_modal">here</a>\
+                          to manage it and renew it!', 'attention');
+
+              if (isPastDate(closed)){
+                the_date  = moment(closed).format("YYYY-MM-DD HH:mm");
+              }
+
+              s_class   = 'in_red';
+              s_message = 'expired';
+              s_icon = "<a href='#' rel='tooltip' title='renew'>\
+                         <span class='glyphicon glyphicon-refresh' onClick=renew_slice('"+i+"','"+slicename+"');></span>\
+                       </a>";
+            }
+
+            $(body).append("<div class='row'>\
+                              <div class='col-md-6'>"+slicename+"</div>\
+                              <div class='col-md-4' id='datetime_expiration"+i+"'>\
+                                <span class="+s_class+">"+the_date+"<span>\
+                              </div>\
+                              <div class='col-md-2' id='icon_"+i+"'>"+s_icon+"</div>\
+                            </div>");
+            $('a').tooltip();
+          }
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+          s_message = "slice not available or does not exist";
+          s_class   = 'in_red';
+          s_icon = "<a href='#' rel='popover' title='"+s_message+"'>\
+                     <span class='glyphicon glyphicon-ban-circle "+s_class+"'></span>\
+                   </a>";
+          $(body).append("<div class='row'>\
+                            <div class='col-md-6 "+s_class+"'>"+value+"</div>\
+                            <div class='col-md-4' id='datetime_expiration"+i+"'>\
+                              <span class="+s_class+"><span>\
+                            </div>\
+                            <div class='col-md-2' id='icon_"+i+"'>"+s_icon+"</div>\
+                          </div>");
+            $('a').tooltip();
+        }
+      });
+    });
+  }
 
   function main(){
     partial();
-    get_slices("list-slices", r2lab_slices);
-    console.log(r2lab_slices);
+    try_slices("list-slices", ["onelab.inria.oai.oai_build", "onelab.upmc.fit_demo.wireless_fit", "onelab.inria.r2lab.admin", "onelab.inria.mario.tutorial", "onelab.inria.testwd.walidtest", "onelab.inria.testwd.another_slice"]);
   }
-
 
   main();
 });
