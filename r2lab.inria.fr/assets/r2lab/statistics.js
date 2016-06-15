7946ed88286203c8e2def3f2a30f6abd985640a3
Mario Zancanaro <nano@creaator.com>
2016-06-15 16:10:01 +0200

    Initial chart page...

---
+++ r2lab.inria.fr/assets/r2lab/statistics.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+  var version = '1.0';
+
+
+
+  // gets the json file from nigthly routine
+  var get_file = function() {
+    var request = {
+      "file" : 'nigthly'
+    };
+    post_omfrest_request('/files/get', request, function(xhttp) {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        var answer = JSON.parse(xhttp.responseText);
+        $.each(answer, function (index, value) {
+          console.log(value);
+        });
+      }
+    });
+  }
+
+
+  var week_chart = function() {
+    var randomScalingFactor = function() {
+        return (Math.random() > 0.5 ? 1.0 : 1.0) * Math.round(Math.random() * 100);
+    };
+    var randomColorFactor = function() {
+        return Math.round(Math.random() * 255);
+    };
+    var randomColor = function() {
+        return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',.7)';
+    };
+
+    var barChartData = {
+        labels: range(1,37),
+        datasets: [{
+            label: '[10-17] Jun 2016',
+            backgroundColor: "rgba(220,220,220,0.5)",
+            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        // }, {
+        //     hidden: true,
+        //     label: 'Dataset 2',
+        //     backgroundColor: "rgba(151,187,205,0.5)",
+        //     data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        // }, {
+        //     label: 'Dataset 3',
+        //     backgroundColor: "rgba(151,187,205,0.5)",
+        //     data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        // }]
+      }]
+    };
+
+    window.onload = function() {
+        var ctx = document.getElementById("canvas").getContext("2d");
+        window.myBar = new Chart(ctx, {
+            type: 'bar',
+            data: barChartData,
+            options: {
+                // Elements options apply to all of the options unless overridden in a dataset
+                // In this case, we are setting the border of each bar to be 2px wide and green
+                elements: {
+                    rectangle: {
+                        borderWidth: 2,
+                        borderColor: 'rgb(0, 255, 0)',
+                        borderSkipped: 'bottom'
+                    }
+                },
+                responsive: true,
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'semanal statistics'
+                },
+                scales: {
+                  yAxes: [{
+                    scaleLabel: {
+                      display: true,
+                      labelString: '% of issues detected'
+                    },
+
+                    ticks: {
+                        min: 0,
+                        max: 100,
+                        beginAtZero: true
+                      }
+
+                  }],
+                  xAxes: [{
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'nodes'
+                    }
+                  }]
+                }
+            }
+        });
+    };
+  }
+
+  function range(j, k) {
+    return Array
+        .apply(null, Array((k - j) + 1))
+        .map(function(discard, n){ return n + j; });
+  }
+
+
+  function main(){
+    console.log("liveslices version " + version);
+    get_file();
+
+    week_chart();
+  }
+
+
+
+  main();
+});
---
+++ r2lab.inria.fr/markdown/statistics.md
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="/assets/r2lab/omfrest.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="/assets/r2lab/statistics.js"></script>
+<script src="/assets/js/chartlib/dist/Chart.bundle.js"></script>
+
+<div style="width: 70%">
+	<canvas id="canvas" height="450" width="700"></canvas>
+</div>
