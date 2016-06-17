d261e2336624158748c8c12a6afe09d57fce6d92
Mario Zancanaro <nano@creaator.com>
2016-06-17 13:16:39 +0200

    Calendar picker

---
+++ r2lab.inria.fr/assets/r2lab/range-calendar.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#range_calendar').daterangepicker({
+      "ranges": {
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      },
+      "minDate": "01/22/2016",
+      "opens": "left",
+  }, function(start, end, label) {
+    // console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
+  });
+});
