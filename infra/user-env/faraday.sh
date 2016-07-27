1aba157049b9f306e2a4a737e396a2112b8b1623
Mario Zancanaro <nano@creaator.com>
2016-07-27 14:19:08 +0200

    update immediately maintenance in the graphs

--- infra/user-env/faraday.sh
+++ infra/user-env/faraday.sh
@@ -164,6 +164,7 @@ function maintenance () {
     python /root/r2lab/nightly/maintenance.py "$@"
     if [ $? -eq 0 ]; then
       /root/r2lab/infra/scripts/sync-nightly-results-at-r2lab.sh
+      /root/r2lab/infra/scripts/restart-website.sh
       echo 'send to r2lab...'
     else
       echo 'Ops! Something went wrong in maintenance command.'
