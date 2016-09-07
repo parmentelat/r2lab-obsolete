b792e0225e70266b40ee8fc694616cafe1f6b0a0
Mario Zancanaro <nano@creaator.com>
2016-09-07 11:31:23 +0200

    Update in book nightly

--- nightly/book-nightly.py
+++ nightly/book-nightly.py
@@ -1,23 +1,33 @@
 #/usr/bin/env python3
-# sample code to book leases
-# expected to run on either faraday or r2lab
+#
+# Author file: Mario Zancanaro <mario.zancanaro@inria.fr>
+#
+"""
+The book-nightly is a script used to book leases for the nightly routine which runs in faraday
+The code will search for all WEDNESDAYs and SUNDAYs between a given period to schedule 1 hour task (3am until 4am)
+in each day found.
+If no period is given, the whole year period is assumed based in the current year.
+
+Be careful in use a different slice name because: onelab.inria.r2lab.nightly is hardcoded in run/book r2lab pages
+to present special contents linked with this name.
+"""
 
 import os.path
 import uuid
 import asyncio
 from argparse import ArgumentParser
 from rhubarbe.omfsfaproxy import OmfSfaProxy
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
+from itertools import islice
 
 
 parser = ArgumentParser()
-parser.add_argument("-b", "--begin", dest="begin",
-                    help="Initial slice date time (Ex. 2016-10-10T10:00:00Z)")
-parser.add_argument("-e", "--end", dest="end",
-                    help="Final slice date time (Ex. 2016-10-10T10:00:00Z)")
+parser.add_argument("-p", "--period", dest="period", nargs=2, type=str, default=[None,None],
+                    help="Each WED and SUN between a given period")
 parser.add_argument("-s", "--slice", dest="slice", default="onelab.inria.r2lab.nightly",
                     help="Slice name")
+
 args = parser.parse_args()
 
 
@@ -54,38 +64,71 @@ def co_add_lease(slicename, valid_from, valid_until):
         'components' : [ {'uuid' : node_uuid } ],
     }
 
-    print("INFO: omf_sfa POST request {}".format(lease_request))
+    # print("INFO: omf_sfa POST request {}".format(lease_request))
     result = yield from omf_sfa_proxy.REST_as_json("leases", "POST", lease_request)
-    print("INFO: omf_sfa POST -> {}".format(result))
+    # print("INFO: omf_sfa POST -> {}".format(result))
     return result
 
 
 
 def format_date(val=None):
-    """ current date (2016-09-05T15:29:18Z)
+    """ return formated date
     """
     if val is None:
-        return datetime.now().strftime("%Y-%m-%d %H:%M")
+        return str(datetime.now().strftime("%Y-%m-%d"))
     else:
-        return str(datetime.strptime(val, "%Y-%m-%d %H:%M").date())
+        print('ta')
+        return str(datetime.strptime(val, "%Y-%m-%d").date())
+
+
+
+def intersections(weekday, start_date=None, end_date=None):
+    """ generate dates where the given weekday and day of month intersect.
+    If the optional start_date is not given, uses the value of the datetime.today().
+    """
+    dates = []
+    if start_date is None:
+        date = datetime.today()
+    else:
+        date = start_date
+    while date.strftime('%F%H%M%S') <= end_date.strftime('%F%H%M%S'):
+        if date.isoweekday() == weekday:
+            dates.append(date)
+        date += timedelta(days=1)
+
+    return dates
 
 
 
 def main(args):
     """
     """
-    begin = args.begin
-    end   = args.end
-    slice = args.slice
+    period_begin = args.period[0]
+    period_end   = args.period[1]
+    slice        = args.slice
 
-    if (begin is None or end is None or slice is None):
+    if period_begin is None:
+        period_begin = datetime(datetime.today().year,  1, 1 )
+    if period_end is None:
+        period_end   = datetime(datetime.today().year, 12, 31)
+
+    WEDNESDAY = 3
+    SUNDAY    = 7
+
+    if (period_begin is None or period_end is None or slice is None):
         print("ERROR: slice name, begin and final date must be present.")
         exit()
     else:
-        print("INFO: slice {} starting at: {}, and finishing at: {}.".format(slice, begin,end))
-        loop = asyncio.get_event_loop()
-        js = loop.run_until_complete(co_add_lease(slice, begin, end))
-        # print(js)
+        wed_occurrences = intersections(WEDNESDAY, period_begin, period_end)
+        sun_occurrences = intersections(SUNDAY   , period_begin, period_end)
+        for occurrence in wed_occurrences + sun_occurrences:
+            slice_beg = occurrence + timedelta(hours=3)
+            slice_end = slice_beg  + timedelta(hours=1)
+
+            # loop = asyncio.get_event_loop()
+            # js = loop.run_until_complete(co_add_lease(slice, slice_beg, slice_end))
+
+        print("INFO: slices {} between {}, and {} were added.".format(slice, format_date(period_begin), format_date(period_end)))
 
 
 
