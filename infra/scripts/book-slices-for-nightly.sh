b68fa474c4bf12b3cf9316c150845fd0ba3e124f
Mario Zancanaro <nano@creaator.com>
2016-09-08 15:24:14 +0200

    Comments, examples and final script

--- infra/scripts/book-slices-for-nightly.sh
+++ infra/scripts/book-slices-for-nightly.sh
@@ -1,4 +1,17 @@
 SHELL=/bin/sh
 HOME=/root
 PATH=/root/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
-python3 /root/r2lab/nightly/book-nightly.py -p 2016-9-8 2016-9-18 -d sun,wed > /var/log/book-nightly.log 2>&1;
+
+#WE ARE CALLING THIS SCRIPT ONCE A YEAR, at 01/01/YYYY 00:00 FROM FARADAYS CRONTAB
+
+# Book each SUNDAYs and WEDNESDAYs in the whole current year (default when not date peiord is given)
+python3 /root/r2lab/nightly/book-nightly.py -d sun,wed > /var/log/book-nightly.log 2>&1;
+
+# EXAMPLES
+
+# An example considering an specific date period and SUNDAYs and WEDNESDAYs
+# If no weekday is given it will book every day
+#python3 /root/r2lab/nightly/book-nightly.py -p 2016-9-8 2016-9-18 -d sun,wed > /var/log/book-nightly.log 2>&1;
+
+# An example to book every day for the whole current year
+#python3 /root/r2lab/nightly/book-nightly.py > /var/log/book-nightly.log 2>&1;
