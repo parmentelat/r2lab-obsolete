3f23215302778936592a0235e9e55e00a23ca933
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-09-28 11:53:17 +0200

    pull-from-preplab can be run with first arg=reverse

--- rhubarbe-images/pull-from-preplab.sh
+++ rhubarbe-images/pull-from-preplab.sh
@@ -7,8 +7,20 @@ preplab=bemol.pl.sophia.inria.fr
 
 hostname | grep -q faraday || { echo "Must run on faraday"; exit 1; }
 
+case "$1" in
+    push|reverse)
+	shift
+	from=$repo/
+	to=$preplab:$repo/
+	;;
+    *)
+	from=$preplab:$repo/
+	to=$repo/
+	;;
+esac
+    
 # Thierry - Apr 26 2016 : 
 # it is much safer to *NOT* mention --delete
 # as images can be saved directly on faraday 
-rsync "$@" -av --exclude $this --exclude archive --exclude \*.log --exclude root-node\* --exclude saving\* $preplab:$repo/ $repo/ 
+rsync "$@" -av --exclude $this --exclude archive --exclude \*.log --exclude root-node\* --exclude saving\* $from $to
 
