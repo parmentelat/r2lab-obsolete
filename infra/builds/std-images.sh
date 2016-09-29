db3535a6c8dd4cdf5a33ba23ab3143183ca0d635
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-09-29 18:47:11 +0200

    a single build script is good enough

--- infra/builds/std-images.sh
+++ infra/builds/std-images.sh
@@ -9,19 +9,32 @@ gateway=root@faraday.inria.fr
 cd $gitroot/infra/user-env
 
 # ew don't need all this but it makes it easier
-alias b="$build $gateway -i oai-common.sh -i nodes.sh -i common.sh"
+function b () {
+    $build $gateway -i oai-common.sh -i nodes.sh -i common.sh "$@"
+}
 
 # augment ubuntu-16.04 with ntp
 b fit01 ubuntu-16.04-v4-node-env ubuntu-16.04-v5-ntp \
-  "imaging.sh ubuntu-setup-ntp"
+  "imaging.sh ubuntu-setup-ntp" \
+  "nodes.sh gitup"
 
 # same on ubuntu-14.04 + node-env
 b fit02 ubuntu-14.04-v3-stamped ubuntu-14.04-v4-ntp-node-env \
   "imaging.sh ubuntu-setup-ntp" \
   "imaging.sh common-setup-user-env" \
-  "imaging.sh common-setup-node-ssh-key"
-
+  "imaging.sh common-setup-node-ssh-key" \
+  "nodes.sh gitup"
 
 # same on fedora-23
 b fit03 fedora-23-node-env fedora-23-v4-ntp \
   "imaging.sh fedora-setup-ntp" \
+  "nodes.sh gitup"
+
+# create base image for OAI gateway (infra) on ubuntu-16
+b fit04 ubuntu-16.04 oai16-gw-base "oai-gw.sh image" 
+
+# create base image for OAI gateway (infra) on ubuntu-14
+b fit05 ubuntu-14.04 oai14-gw-base "oai-gw.sh image" 
+
+# try to run apt-upgrade-all on ubuntu-16
+b fit06 ubuntu-16.04 u16-upgrade "nodes.sh apt-upgrade-all"
