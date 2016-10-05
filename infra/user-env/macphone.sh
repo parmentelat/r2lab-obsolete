9a317121d73f98b5920040bbd67f7383e8ba45b1
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-10-05 15:27:52 +0200

    trying another angle

--- infra/user-env/macphone.sh
+++ infra/user-env/macphone.sh
@@ -3,7 +3,8 @@
 
 source $(dirname "$BASH_SOURCE")/r2labutils.sh
 
-type -p adb >& /dev/null || alias adb="$HOME/nexustools/adb"
+adb="$HOME/nexustools/adb"
+[ -x $adb ] || echo "WARNING: from $BASH_SOURCE : $adb not executable"
 
 create-doc-category phone "tools for managing R2lab phone from macphone"
 augment-help-with phone
@@ -11,18 +12,18 @@ augment-help-with phone
 doc-phone phone-on "turn off airplane mode"
 function phone-on() {
     echo "Turning ON phone - i.e. turning off airplane mode"
-    adb shell "settings put global airplane_mode_on 0; am broadcast -a android.intent.action.AIRPLANE_MODE --ez state false"
+    $adb shell "settings put global airplane_mode_on 0; am broadcast -a android.intent.action.AIRPLANE_MODE --ez state false"
 }
 
 doc-phone phone-off "turn off airplane mode"
 function phone-off() {
     echo "Turning OFF phone - i.e. turning on airplane mode"
-    adb shell "settings put global airplane_mode_on 1; am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true"
+    $adb shell "settings put global airplane_mode_on 1; am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true"
 }
 
 doc-phone phone-status "shows wheter airplane mode is on or off"
 function phone-status() {
-    airplane_mode_on=$(adb shell "settings get global airplane_mode_on")
+    airplane_mode_on=$($adb shell "settings get global airplane_mode_on")
     airplane_mode_on=$(echo $airplane_mode_on)
     case $airplane_mode_on in
 	0*) echo phone is turned ON ;;
@@ -34,8 +35,8 @@ function phone-status() {
 doc-phone phone-reboot "reboot phone with abd reboot"
 function phone-reboot() {
     echo "REBOOTING phone ..."
-    #    adb shell am broadcast -a android.intent.action.BOOT_COMPLETED
-    adb reboot
+    #    $adb shell am broadcast -a android.intent.action.BOOT_COMPLETED
+    $adb reboot
 }
 
 doc-phone refresh "retrieve latest git repo, and source it in this shell"
@@ -46,8 +47,8 @@ function refresh() {
 }
 
 # to set LTE only - except that sqlite3 is not known
-#adb shell sqlite3 /data/data/com.android.providers.settings/databases/settings.db "update global SET value=11 WHERE name='preferred_network_mode'"
-#adb shell sqlite3 /data/data/com.android.providers.settings/databases/settings.db "select value FROM secure WHERE name='preferred_network_mode'"
+#$adb shell sqlite3 /data/data/com.android.providers.settings/databases/settings.db "update global SET value=11 WHERE name='preferred_network_mode'"
+#$adb shell sqlite3 /data/data/com.android.providers.settings/databases/settings.db "select value FROM secure WHERE name='preferred_network_mode'"
 
 ########################################
 define-main "$0" "$BASH_SOURCE" 
