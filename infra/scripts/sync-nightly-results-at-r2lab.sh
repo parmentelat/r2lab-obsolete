595acc0f4808173b5af281227bd8aefe202cc2ae
Mario Zancanaro <nano@creaator.com>
2016-10-10 15:23:48 +0200

    Sync folders

--- infra/scripts/sync-nightly-results-at-r2lab.sh
+++ infra/scripts/sync-nightly-results-at-r2lab.sh
@@ -7,5 +7,5 @@ rsync -a /root/r2lab/nightly/nightly_data.json root@r2lab.inria.fr:/root/r2lab/r
 rsync -a /root/r2lab/nodes/maintenance_nodes.json root@r2lab.inria.fr:/root/r2lab/r2lab.inria.fr/files/nodes/ > /var/log/sync-maintenance.log 2>&1;
 ##sync info db, images and md files
 rsync -a /root/r2lab/nodes/info_nodes.json root@r2lab.inria.fr:/root/r2lab/r2lab.inria.fr/files/nodes/ > /var/log/sync-info.log 2>&1;
-rsync -a /root/r2lab/nodes/images/* root@r2lab.inria.fr:/root/r2lab/r2lab.inria.fr/files/nodes/images/ > /var/log/sync-info-images.log 2>&1;
-rsync -a /root/r2lab/nodes/info/* root@r2lab.inria.fr:/root/r2lab/r2lab.inria.fr/files/nodes/info/ > /var/log/sync-info-files.log 2>&1;
+rsync -rtu --delete /root/r2lab/nodes/images/ root@r2lab.inria.fr:/root/r2lab/r2lab.inria.fr/files/nodes/images/ > /var/log/sync-info-images.log 2>&1;
+rsync -rtu --delete /root/r2lab/nodes/info/ root@r2lab.inria.fr:/root/r2lab/r2lab.inria.fr/files/nodes/info/ > /var/log/sync-info-info.log 2>&1;
