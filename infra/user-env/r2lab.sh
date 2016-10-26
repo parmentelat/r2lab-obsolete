6e9da2cc8b01fd27917629b75e1b2cb23149dcd4
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-10-26 19:37:11 +0200

    logs-sidecar needs to use journalctl

--- infra/user-env/r2lab.sh
+++ infra/user-env/r2lab.sh
@@ -4,4 +4,4 @@
 alias logs-django="tail -f /var/lib/r2lab.inria.fr/django.log /var/log/httpd/*log"
 alias logs-r2lab=logs-django
 
-alias logs-sidecar="tail -f /var/log/sidecar.log"
+alias logs-sidecar="journalctl -b -f --unit=sidecar"
