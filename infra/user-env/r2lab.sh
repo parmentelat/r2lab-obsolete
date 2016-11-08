0241c21af247398d4fd3cd9b3c2df954b4051096
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-11-08 09:27:29 +0100

    fixes the doc-admin issue in r2lab shell profile

--- infra/user-env/r2lab.sh
+++ infra/user-env/r2lab.sh
@@ -1,6 +1,12 @@
 # convenience tools for r2lab.inria.fr
 #
 # not sure where this log file is going to end up
+
+# use the micro doc-help tool
+source $(dirname $(readlink -f $BASH_SOURCE))/r2labutils.sh
+
+create-doc-category admin "admin-oriented commands"
+
 alias logs-django="tail -f /var/lib/r2lab.inria.fr/django.log /var/log/httpd/*log"
 doc-admin logs-django alias
 alias logs-r2lab=logs-django
