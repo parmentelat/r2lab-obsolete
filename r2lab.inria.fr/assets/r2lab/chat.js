03142cb62dbd647b66302ebe494ad5fd9c461559
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-12-02 12:11:23 +0100

    was meant in previous commit

--- r2lab.inria.fr/assets/r2lab/chat.js
+++ r2lab.inria.fr/assets/r2lab/chat.js
@@ -4,7 +4,7 @@ $(function() {
 	this.init = function() {
 	    console.log($("#chat-container"));
 	    $("#chat-container").html(
-		'<button class="chat-on-off">Chat room on and off</button>'
+		'<button class="chat-on-off">Join chat room #r2lab at freenode (on and off)</button>'
 		    + '<iframe style="display:none" class="chat-body" src="http://webchat.freenode.net?nick='
 		    + this.username
 		    + '&channels=%23r2lab&uio=MTY9dHJ1ZSY5PXRydWUmMTE9MzA572" width="100%" height="400"></iframe>');
