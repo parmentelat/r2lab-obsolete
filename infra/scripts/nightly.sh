05ebba5aff175aad072e216a1a61da51497982ca
Mario Zancanaro <nano@creaator.com>
2017-01-09 14:22:00 +0100

    update control of nightly

--- infra/scripts/nightly.sh
+++ infra/scripts/nightly.sh
@@ -1,4 +1,7 @@
 SHELL=/bin/sh
 HOME=/root
 PATH=/root/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
-python /root/r2lab/nightly/nightly.py -N all > /var/log/nightly.log 2>&1;
+#python /root/r2lab/nightly/nightly.py -N all > /var/log/nightly.log 2>&1;
+#Now I call the fetch to check if a nightly lease is schedule once
+#the users can move it in the UI
+python /root/r2lab/nightly/fetch-nightly.py -N all > /var/log/fetch-nightly.log 2>&1;
