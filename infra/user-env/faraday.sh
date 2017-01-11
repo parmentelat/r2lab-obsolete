7383b10ae359f6bb83364d25387f8ce8e7847775
Mario Zancanaro <nano@creaator.com>
2017-01-11 12:48:58 +0100

    a little confirmation

--- infra/user-env/faraday.sh
+++ infra/user-env/faraday.sh
@@ -693,7 +693,10 @@ doc-selection-sep "See also help-alt for other commands"
 ########################################
 doc-admin maintenance "run nightly routine | nightly -N all | nightly -N <nodes> -e <email result> -a <avoid nodes>"
 function nightly () {
-  python /root/r2lab/nodes/nightly.py "$@"
+  read -p "run nightly? (y/n)" CONT
+  if [ "$CONT" = "y" ]; then
+    python /root/r2lab/nodes/nightly.py "$@"
+  fi
 }
 
 doc-admin maintenance "update a json file which contains information about nodes maintenance | maitenance -i|r <node> -m <the message>"
