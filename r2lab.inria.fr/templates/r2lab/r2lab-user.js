3fd99337e2ab01392434c5ba163106503de7e6ab
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-01-18 09:20:37 +0100

    cosmetic - js uses ‘var’ to define variables

--- r2lab.inria.fr/templates/r2lab/r2lab-user.js
+++ r2lab.inria.fr/templates/r2lab/r2lab-user.js
@@ -2,11 +2,11 @@
 <script type="text/javascript" src="/assets/r2lab/current-slice.js"></script>
 <script type="text/javascript">
 // globals that describe logged in user 
-r2lab_user = {{r2lab_context.user_details|jsonify|safe}};
+var r2lab_user = {{r2lab_context.user_details|jsonify|safe}};
 // shortcuts
-r2lab_email = {{r2lab_context.user_details.email|jsonify|safe}};
-r2lab_hrn = {{r2lab_context.user_details.hrn|jsonify|safe}};
-r2lab_accounts = {{r2lab_context.accounts|jsonify|safe}};
+var r2lab_email = {{r2lab_context.user_details.email|jsonify|safe}};
+var r2lab_hrn = {{r2lab_context.user_details.hrn|jsonify|safe}};
+var r2lab_accounts = {{r2lab_context.accounts|jsonify|safe}};
 // initialize current_slice
 // javascript code should use 'current_slice.name'
 current_slice.init_from_storage(r2lab_accounts);
