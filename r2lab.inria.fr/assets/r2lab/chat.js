1c459bc11e08e9c3c21008453c9abd57e8313731
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-01-24 17:53:37 +0100

    use https to reach freenode since now r2lab runs on https

--- r2lab.inria.fr/assets/r2lab/chat.js
+++ r2lab.inria.fr/assets/r2lab/chat.js
@@ -5,7 +5,7 @@ $(function() {
 	    console.log($("#chat-container"));
 	    $("#chat-container").html(
 		'<a class="chat-on-off btn btn-primary btn-large">Join chat room #r2lab at freenode (on and off)</a>'
-		    + '<iframe style="display:none" class="chat-body" src="http://webchat.freenode.net?nick='
+		    + '<iframe style="display:none" class="chat-body" src="https://webchat.freenode.net?nick='
 		    + this.username
 		    + '&channels=%23r2lab&uio=MTY9dHJ1ZSY5PXRydWUmMTE9MzA572"'
 		    + ' width="100%" height="400"></iframe>');
