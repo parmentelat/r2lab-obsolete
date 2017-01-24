66dcf8273cdfd3c75447e920f6050dff7e8bf0ea
Mario Zancanaro <nano@creaator.com>
2017-01-24 12:55:22 +0100

    cmd changes to p3

--- infra/scripts/inspect.sh
+++ infra/scripts/inspect.sh
@@ -2,4 +2,4 @@ SHELL=/bin/sh
 HOME=/root
 PATH=/root/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
 
-python /root/r2lab/infra/inspect/inspect.py > /var/log/inspect.log 2>&1;
+python3 /root/r2lab/infra/inspect/inspect.py > /var/log/inspect.log 2>&1;
