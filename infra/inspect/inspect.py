c7416b959645a3bc3e4c6b90956ec60f5a6475ee
Mario Zancanaro <nano@creaator.com>
2017-02-07 10:41:16 +0100

    timeout is not necessary anymore

--- infra/inspect/inspect.py
+++ infra/inspect/inspect.py
@@ -42,21 +42,6 @@ send_to = args.send_to
 domains = args.domains
 force_e = args.force_email
 
-#TO DEAL WITH NO ANSWER AFTER x SECONDS TRYING CONNECT SIDECAR
-import signal
-class timeout:
-    def __init__(self, seconds=1, error_message='Timeout'):
-        self.seconds = seconds
-        self.error_message = error_message
-    def handle_timeout(self, signum, frame):
-        raise TimeoutError(self.error_message)
-    def __enter__(self):
-        signal.signal(signal.SIGALRM, self.handle_timeout)
-        signal.alarm(self.seconds)
-    def __exit__(self, type, value, traceback):
-        signal.alarm(0)
-
-
 
 def main():
     inspect_sites(domains)
