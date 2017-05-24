851246a4a939451adb881b76db6cca2ad0d26b42
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-05-24 16:12:31 +0200

    the 3 kinds of column-oriented display use javascript classes

--- r2lab.inria.fr/assets/r2lab/livehardware.js
+++ r2lab.inria.fr/assets/r2lab/livehardware.js
@@ -14,100 +14,100 @@ let livehardware_options = {
 //////////////////////////////
 // nodes are dynamic
 // their hardware row and cells get created through d3 enter mechanism
-let LiveHardwareNode = function (id) {
+class LiveHardwareNode extends LiveColumnsNode{
 
-    LiveColumnsNode.prototype.constructor(id);
-
-    this.cells_data = [
-	[ span_html(id, 'badge pointer'), '' ],	// id
-	undefined,				// avail
-	undefined,				// on/off
-	undefined,				// usrp-on-off
-	undefined,				// duplexer
-	undefined,				// usrp-antenna
-	undefined,				// wifi antennas
-    ];
-}
-
-LiveHardwareNode.prototype.__proto__ = LiveColumnsNode.prototype;
-
-// nodes worth being followed when clicking on the hardware banner
-LiveHardwareNode.prototype.is_worth = function() {
-    return (   (this.usrp_type || 'none') != 'none');
-}
-
-// after the internal properties are updated from the incoming JSON message
-// we need to rewrite actual representation in cells_data
-// that will contain a list of ( html_text, class )
-// used by the d3 mechanism to update the <td> elements in the row
-LiveHardwareNode.prototype.compute_cells_data = function() {
-    let col = 1
-    // avail
-    this.cells_data[col++] = this.cell_available();
-    // on-off
-    this.cells_data[col++] = this.cell_on_off();
-    // usrp-on-off
-    this.cells_data[col++] = this.cell_usrp();
-    // duplexer details
-    this.cells_data[col++] = this.cell_duplexer();
-    // usrp antenna(s) images
-    this.cells_data[col++] = this.cell_usrp_antennas();
-    // wifi antenna(s) images
-    this.cells_data[col++] = this.cell_wifi_antennas();
-}
-
-LiveHardwareNode.prototype.cell_duplexer = function() {
-    let html = (! 'usrp_duplexer' in this) ? '-' : this.usrp_duplexer;
-    return [ html, "" ];
-}
-
-LiveHardwareNode.prototype.image_link = function(img) {
-    // xxx where to store those files exactly ?
-    let icon = span_html('', 'fa fa-camera');
-    // something like ${this.id:02d} 
-    // let str_id = (this.id <= 9) ? `0${this.id}` : `${this.id}`;
-    return `<a class='image-link' alt="click to see image" onclick='show_image("/raw/node-images/${img}")'>${icon}</a>`;
-}
-
-LiveHardwareNode.prototype.cell_usrp_antennas = function() {
-    if ((!('images_usrp' in this)) || (this.images_usrp.length == 0)) {
-	return '-';
+    constructor(id) {
+	super(id);
+	this.cells_data = [
+	    [ span_html(id, 'badge pointer'), '' ],	// id
+	    undefined,				// avail
+	    undefined,				// on/off
+	    undefined,				// usrp-on-off
+	    undefined,				// duplexer
+	    undefined,				// usrp-antenna
+	    undefined,				// wifi antennas
+	];
     }
-    let html = this.images_usrp.map( (name) => this.image_link(name)).join(" / ");
-    return [html, "image-links"]
+
+    // nodes worth being followed when clicking on the hardware banner
+    is_worth() {
+	return (   (this.usrp_type || 'none') != 'none');
+    }
+
+    // after the internal properties are updated from the incoming JSON message
+    // we need to rewrite actual representation in cells_data
+    // that will contain a list of ( html_text, class )
+    // used by the d3 mechanism to update the <td> elements in the row
+    compute_cells_data() {
+	let col = 1
+	// avail
+	this.cells_data[col++] = this.cell_available();
+	// on-off
+	this.cells_data[col++] = this.cell_on_off();
+	// usrp-on-off
+	this.cells_data[col++] = this.cell_usrp();
+	// duplexer details
+	this.cells_data[col++] = this.cell_duplexer();
+	// usrp antenna(s) images
+	this.cells_data[col++] = this.cell_usrp_antennas();
+	// wifi antenna(s) images
+	this.cells_data[col++] = this.cell_wifi_antennas();
+    }
+
+    cell_duplexer() {
+	let html = (! 'usrp_duplexer' in this) ? '-' : this.usrp_duplexer;
+	return [ html, "" ];
+    }
+
+    image_link(img) {
+	// xxx where to store those files exactly ?
+	let icon = span_html('', 'fa fa-camera');
+	// something like ${this.id:02d} 
+	// let str_id = (this.id <= 9) ? `0${this.id}` : `${this.id}`;
+	return `<a class='image-link' alt="click to see image" onclick='show_image("/raw/node-images/${img}")'>${icon}</a>`;
+    }
+
+    cell_usrp_antennas() {
+	if ((!('images_usrp' in this)) || (this.images_usrp.length == 0)) {
+	    return '-';
+	}
+	let html = this.images_usrp.map( (name) => this.image_link(name)).join(" / ");
+	return [html, "image-links"]
+    }
+
+    cell_wifi_antennas() {
+	if ((!('images_wifi' in this)) || (this.images_wifi.length == 0)) {
+	    return '-';
+	}
+	let html = this.images_wifi.map( (name) => this.image_link(name)).join(" / ");
+	return [html, "image-links"]
+    }
+    
 }
 
-LiveHardwareNode.prototype.cell_wifi_antennas = function() {
-    if ((!('images_wifi' in this)) || (this.images_wifi.length == 0)) {
-	return '-';
-    }
-    let html = this.images_wifi.map( (name) => this.image_link(name)).join(" / ");
-    return [html, "image-links"]
-}
 
 //////////////////////////////
-function LiveHardware(domid) {
+class LiveHardware extends LiveColumns {
 
-    LiveColumns.prototype.constructor(this);
-    this.domid = domid;
-}
+    constructor(domid) {
+	super();
+	this.domid = domid;
+    }
 
-// inheritance
-LiveHardware.prototype.__proto__ = LiveColumns.prototype;
+    init_headers(header) {
+	header.append('th').html('node');
+	header.append('th').html('avail.');
+	header.append('th').html('on/off');
+	header.append('th').html('usrp');
+	header.append('th').html('duplexer');
+	header.append('th').html('usrp antenna');
+	header.append('th').html('wifi antennas');
+    }
 
-LiveHardware.prototype.init_headers = function (header) {
-    header.append('th').html('node');
-    header.append('th').html('avail.');
-    header.append('th').html('on/off');
-    header.append('th').html('usrp');
-    header.append('th').html('duplexer');
-    header.append('th').html('usrp antenna');
-    header.append('th').html('wifi antennas');
-}
-
-LiveHardware.prototype.init_nodes = function () {
-    for (let i=0; i < livecolumns_options.nb_nodes; i++) {
-	this.nodes[i] = new LiveHardwareNode(i+1);
+    init_nodes() {
+	for (let i=0; i < livecolumns_options.nb_nodes; i++) {
+	    this.nodes[i] = new LiveHardwareNode(i+1);
+	}
     }
 }
 
@@ -118,7 +118,7 @@ $(function() {
     the_livehardware.init();
 })
 
-// helpers
+//////////////////// helpers
 function show_image(img) {
     $('#big_image_content').html('<img src="'+img+'" class="max-img" >');
     $('#big_photo').modal('toggle');
--- r2lab.inria.fr/assets/r2lab/livetable.js
+++ r2lab.inria.fr/assets/r2lab/livetable.js
@@ -18,117 +18,117 @@ let livetable_options = {
 //////////////////////////////
 // nodes are dynamic
 // their table row and cells get created through d3 enter mechanism
-let LiveTableNode = function (id) {
+class LiveTableNode extends LiveColumnsNode {
 
-    LiveColumnsNode.prototype.constructor(id);
-    this.cells_data = [
-	[ span_html(id, 'badge pointer'), '' ],	// id
-	undefined,				// avail
-	undefined,				// on/off
-	undefined,				// usrp-on-off
-	undefined,				// ping
-	undefined,				// ssh
-	undefined,				// os_release
-	undefined,				// uname
-	undefined				// image_radical
-    ];
-}
+    constructor(id) {
+	super(id);
 
-// inheritance
-LiveTableNode.prototype.__proto__ = LiveColumnsNode.prototype;
+	this.cells_data = [
+	    [ span_html(id, 'badge pointer'), '' ],	// id
+	    undefined,				// avail
+	    undefined,				// on/off
+	    undefined,				// usrp-on-off
+	    undefined,				// ping
+	    undefined,				// ssh
+	    undefined,				// os_release
+	    undefined,				// uname
+	    undefined				// image_radical
+	];
+    }
 
-// nodes worth being followed when clicking on the table banner
-LiveTableNode.prototype.is_worth = function() {
-    return (   this.cmc_on_off == 'on'
-	       || this.usrp_on_off == 'on'
-	       || this.control_ping == 'on'
-	       || this.control_ssh == 'on' )
-	&& this.available != 'ko';
-}
+    // nodes worth being followed when clicking on the table banner
+    is_worth() {
+	return (   this.cmc_on_off == 'on'
+		   || this.usrp_on_off == 'on'
+		   || this.control_ping == 'on'
+		   || this.control_ssh == 'on' )
+	    && this.available != 'ko';
+    }
 
 
-// after the internal properties are updated from the incoming JSON message
-// we need to rewrite actual representation in cells_data
-// that will contain a list of ( html_text, class )
-// used by the d3 mechanism to update the <td> elements in the row
-LiveTableNode.prototype.compute_cells_data = function () {
-    let col = 1
-    // available
-    this.cells_data[col++] = this.cell_available();
-    // on/off
-    this.cells_data[col++] = this.cell_on_off();
-    // usrp
-    this.cells_data[col++] = this.cell_usrp();
-    // ping
-    this.cells_data[col++] =
-	(this.control_ping == 'on')
-	? [ span_html('', 'fa fa-link'), 'ok' ]
-	: [ span_html('', 'fa fa-unlink'), 'ko' ];
-    // ssh
-    this.cells_data[col++] =
-	this.control_ssh == 'on' ? [ span_html('', 'fa fa-circle'), 'ok' ]
-	: [ span_html('', 'fa fa-circle-o'), 'ko' ];
-    //
-    this.cells_data[col++] = this.cell_release(this.os_release);
-    this.cells_data[col++] = this.cell_uname(this.uname);
-    this.cells_data[col++] = this.cell_image(this.image_radical);
-}
+    // after the internal properties are updated from the incoming JSON message
+    // we need to rewrite actual representation in cells_data
+    // that will contain a list of ( html_text, class )
+    // used by the d3 mechanism to update the <td> elements in the row
+    compute_cells_data () {
+	let col = 1
+	// available
+	this.cells_data[col++] = this.cell_available();
+	// on/off
+	this.cells_data[col++] = this.cell_on_off();
+	// usrp
+	this.cells_data[col++] = this.cell_usrp();
+	// ping
+	this.cells_data[col++] =
+	    (this.control_ping == 'on')
+	    ? [ span_html('', 'fa fa-link'), 'ok' ]
+	    : [ span_html('', 'fa fa-unlink'), 'ko' ];
+	// ssh
+	this.cells_data[col++] =
+	    this.control_ssh == 'on' ? [ span_html('', 'fa fa-circle'), 'ok' ]
+	    : [ span_html('', 'fa fa-circle-o'), 'ko' ];
+	//
+	this.cells_data[col++] = this.cell_release(this.os_release);
+	this.cells_data[col++] = this.cell_uname(this.uname);
+	this.cells_data[col++] = this.cell_image(this.image_radical);
+    }
 
-LiveTableNode.prototype.cell_release = function(os_release) {
-    // use a single css class for now
-    let klass = 'os';
-    if (os_release == undefined)
-	return [ "n/a", klass ];
-    if (os_release.startsWith('fedora'))
-	return [ livetable_options.fedora_badge + ' ' + os_release + ' ', klass ];
-    else if (os_release.startsWith('ubuntu'))
-	return [ livetable_options.ubuntu_badge + ' ' + os_release + ' ', klass ];
-    else if (os_release == 'other')
-	return [ livetable_options.other_badge + ' (ssh OK)', klass ];
-    else
-	return [ 'N/A', klass ];
-}
+    cell_release(os_release) {
+	// use a single css class for now
+	let klass = 'os';
+	if (os_release == undefined)
+	    return [ "n/a", klass ];
+	if (os_release.startsWith('fedora'))
+	    return [ livetable_options.fedora_badge + ' ' + os_release + ' ', klass ];
+	else if (os_release.startsWith('ubuntu'))
+	    return [ livetable_options.ubuntu_badge + ' ' + os_release + ' ', klass ];
+	else if (os_release == 'other')
+	    return [ livetable_options.other_badge + ' (ssh OK)', klass ];
+	else
+	    return [ 'N/A', klass ];
+    }
 
-LiveTableNode.prototype.cell_uname = function(uname) {
-    return [ uname, 'os' ];
-}
-
-LiveTableNode.prototype.cell_image = function(image_radical) {
-    let klass = 'image';
-    if (image_radical == undefined)
-	return [ "n/a", klass ];
-    return [ image_radical, klass ];
-}
-
-//////////////////////////////
-function LiveTable(domid) {
-
-    LiveColumns.prototype.constructor(this);
-    this.domid = domid;
-}
-
-// inheritance
-LiveTable.prototype.__proto__ = LiveColumns.prototype;
-
-LiveTable.prototype.init_headers = function (header) {
-    header.append('th').html('node');
-    header.append('th').html('avail.');
-    header.append('th').html('on/off');
-    header.append('th').html('usrp');
-    header.append('th').html('ping');
-    header.append('th').html('ssh');
-    header.append('th').html('last O.S.');
-    header.append('th').html('last uname');
-    header.append('th').html('last image');
-}
-
-LiveTable.prototype.init_nodes = function () {
-    for (let i=0; i < livecolumns_options.nb_nodes; i++) {
-	this.nodes[i] = new LiveTableNode(i+1);
+    cell_uname(uname) {
+	return [ uname, 'os' ];
+    }
+    
+    cell_image(image_radical) {
+	let klass = 'image';
+	if (image_radical == undefined)
+	    return [ "n/a", klass ];
+	return [ image_radical, klass ];
     }
 }
 
-// autoload
+//////////////////////////////
+class LiveTable extends LiveColumns{
+
+    constructor(domid) {
+	super();
+	this.domid = domid;
+    }
+
+    
+    init_headers(header) {
+	header.append('th').html('node');
+	header.append('th').html('avail.');
+	header.append('th').html('on/off');
+	header.append('th').html('usrp');
+	header.append('th').html('ping');
+	header.append('th').html('ssh');
+	header.append('th').html('last O.S.');
+	header.append('th').html('last uname');
+	header.append('th').html('last image');
+    }
+
+    init_nodes() {
+	for (let i=0; i < livecolumns_options.nb_nodes; i++) {
+	    this.nodes[i] = new LiveTableNode(i+1);
+	}
+    }
+}
+
+////////// autoload
 $(function() {
     // name it for debugging from the console
     the_livetable = new LiveTable("livetable_container");
