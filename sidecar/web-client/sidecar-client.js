b1f9d8d344c28d3f0f1f50cffd641b7fc82d2192
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-05-24 13:34:22 +0200

    using javascript ES6

--- sidecar/web-client/sidecar-client.js
+++ sidecar/web-client/sidecar-client.js
@@ -1,12 +1,27 @@
 // the socketio instance
+"use strict";
 
-//var default_url = "http://r2lab.inria.fr:999/";
-//var default_url = "http://localhost:10000/";
-var default_url = "https://localhost:10001/";
+//let default_url = "http://r2lab.inria.fr:999/";
+let devel_url = "http://localhost:10000/";
+let prod_url = "https://r2lab.inria.fr:999/";
+let default_url = devel_url;
 
-var socket = undefined;
+let socket = undefined;
 
-var names = [ 'phones', 'nodes', 'leases'];
+let sections = {
+    phones: {depth: 2,  def_request: 'REQUEST',
+	     def_send: '[{"id":1, "airplane_mode":"on"}]',
+	     prettifier: pretty_records,
+	    },
+    nodes : {depth: 10, def_request: 'REQUEST',
+	     def_send: '[{"id":1, "available":"ko"}]',
+	     prettifier: pretty_records,
+	    },
+    leases: {depth: 2,  def_request: 'REQUEST',
+	     def_send: '-- not recommended --',
+	     prettifier: pretty_leases,
+	    },
+}
 
 //////////////////// global functions
 function show_connected(url) {
@@ -37,25 +52,26 @@ function connect_sidecar(url) {
 
     show_connected(url);
     
-    names.forEach(function(name) {
+    for (let name in sections) {
 	// behaviour for the apply buttons
-	$('div#request-' + name + '>button').click(function(e){
+	$(`div#request-${name}>button`).click(function(e){
 	    send(name, 'request:', 'request-');
 	});
-	$('div#send-' + name + '>button').click(function(e){
+	$(`div#send-${name}>button`).click(function(e){
 	    send(name, 'info:', 'send-');
 	});
 	// what to do upon reception on that channel
-	channel = 'info:' + name;
-	console.log("subscribing to channel " + channel)
+	let channel = 'info:' + name;
+	console.log(`subscribing to channel ${channel}`)
 	socket.on(channel, function(msg){
-	    console.log("received on channel " + channel + " : " + msg);
+	    console.log(`received on channel ${channel} : ${msg}`);
 	    update_contents(name, msg)});
-    })
+    }
 }
 
-var set_url = function(e) {
-    var url = $('input#url').val();
+////////////////////
+let set_url = function(e) {
+    let url = $('input#url').val();
     if (url == "") {
 	url = default_url;
 	$('input#url').val(url);
@@ -63,7 +79,7 @@ var set_url = function(e) {
     connect_sidecar(url);
 }
 
-var pause = function() {
+let pause = function() {
     console.log("Pausing");
     if (socket == undefined) {
 	console.log("already paused");
@@ -74,103 +90,99 @@ var pause = function() {
     show_disconnected();
 }
 
-//////////////////// the 3 channels
-// max number of entries in each section
-var depths = []
-depths['leases'] = 2;
-depths['nodes'] = 10;
-depths['phones'] = 2;
+let set_devel_url = function(e) {
+    $('input#url').val(devel_url);
+    pause();
+    set_url();
+}
 
+let set_prod_url = function(e) {
+    $('input#url').val(prod_url);
+    pause();
+    set_url();
+}
+//////////////////// the 3 channels
 // a function to prettify the leases message
-var pretty_lease = function(json) {
-    console.log("json = " + json);
-    var leases = $.parseJSON(json);
-    var html = "<ul>";
+function pretty_leases(json) {
+    let leases = $.parseJSON(json);
+    let html = "<ul>";
     leases.forEach(function(lease) {
-	var l_html = "<li>";
-	l_html += lease['account']['name'] + " from " + lease['valid_from']
-	    + " until " + lease['valid_until'];
-//	console.log(lease);
-	l_html += "</li>";
-	html += l_html;
+	html +=
+	    `<li>${lease.slicename} from ${lease.valid_from} until ${lease.valid_until}</li>`
     })
     html += "</ul>";
     return html;
 }
 
-var the_record;
 // applicable to nodes and phones
-var pretty_records = function(json) {
-    var records = $.parseJSON(json);
-    console.log("records = " + records);
-    var html = "<ul>";
+function pretty_records(json) {
+    let records = $.parseJSON(json);
+    let html = "<ul>";
     records.forEach(function(record) {
-	var li = "<li>" + JSON.stringify(record) + "</li>";
-	html += li;
-	the_record = record;
+	html += `<li>${JSON.stringify(record)}</li>`;
     });
     html += "</ul>";
     return html;
 }
 
-var prettifiers = []
-prettifiers['phones'] = pretty_records;
-prettifiers['nodes'] = pretty_records;
-prettifiers['leases'] = pretty_lease;
-
 // update the 'contents' <ul> and keep at most <depth> entries in there
 function update_contents(name, value) {
-    var ul_name = '#ul-' + name;
-    var details = value;
-    var prettifier = prettifiers[name];
-    if (prettifier) details = prettifier(details);
-    var html = '<li><span class="date">' + new Date() + '</span>' + details + '</li>';
-    var depth = depths[name];
-    var current = $(ul_name + '>li').length;
-    if (current >= depth) {
-	$('#ul-' + name + '>li').first().remove()
-    } 
-    $('#ul-' + name).append(html);
+    let ul_sel = `#ul-${name}`;
+    let $ul = $(ul_sel);
+    let details = value;
+    let prettifier = sections[name].prettifier;
+    if (prettifier)
+	details = prettifier(details);
+    let html = `<li><span class="date">${new Date()}</span>${details}</li>`;
+    let depth = sections[name].depth;
+    let lis = $(`${ul_sel}>li`);
+    if (lis.length >= depth) {
+	lis.first().remove()
+    }
+    $(ul_sel).append(html);
+}
+
+let clear_all = function() {
+    for (let name in sections) {
+	$(`#ul-${name}`).html("");
+    }
 }
 
     
-var populate = function() {
-    names.forEach(function(name) {
+let populate = function() {
+    for (let name in sections) {
 	// create form for the request input
-	var html;
-	html = '<div class="allpage" id="request-' + name + '"><span class="header">send Request ' + name + '</span>' +
-	    '<input id="' + name + '"/><button>Request update</button></div>';
-	$("#requests").append(html);
-	html = '<div class="allpage" id="send-' + name + '"><span class="header">send raw (json) line as ' + name + '</span>' +
-	    '<input class="wider" id="' + name + '"/><button>Send raw line</button></div>';
-	$("#requests").append(html);
-	// create div for the received contents
-	html = '<div class="contents" id=contents-"' + name + '">' +
-	    '<h3>Contents of ' + name + '</h3>' +
-	    '<ul class="contents" id="ul-' + name + '"></ul>' + '</div>';
+	let html;
+	html = "";
+	html += `<div class="allpage" id="request-${name}">`;
+	html += `<span class="header"> send Request ${name}</span>`;
+	html += `<input id="${name}" /><button class="green">Request update</button>`;
+	html += `</div>`;
+	html += `<div class="allpage" id="send-${name}">`;
+	html += `<span class="header">send raw (json) line as ${name}</span>`;
+	html += `<input class="wider" id="send-${name}" /><button class="red">Send json</button>`;
+	html += `</div>`;
+	$("#controls").append(html);
+	html = "";
+	html += `<div class="contents" id="contents-${name}">`;
+	html += `<h3>Contents of ${name}</h3>`;
+	html += `<ul class="contents" id="ul-${name}"></ul>`;
+	html += `</div>`;
 	$("#contents").append(html);
-    })
-    $("div#send-phones>input").val('[{"id":1, "airplane_mode":"on"}]');
-    $("div#send-nodes>input").val('[{"id":1, "available":"ko"}]');
-    $("div#send-leases>input").val('-- not recommended --');
-    $("div#request-phones>input").val('REQUEST');
-    $("div#request-nodes>input").val('REQUEST');
-    $("div#request-leases>input").val('REQUEST');
+	$(`div#send-${name}>input`).val(sections[name].def_send);
+	$(`div#request-${name}>input`).val(sections[name].def_request);
+    }
 };
 
 // channel_prefix is typically 'info:' or 'request:'
 // widget_prefix is either 'send-' or 'request-'
 function send(name, channel_prefix, widget_prefix) {
-    var channel = channel_prefix + name ;
-    var value = $('#' + widget_prefix + name + ">input").val();
+    let channel = channel_prefix + name ;
+    let value = $('#' + widget_prefix + name + ">input").val();
     console.log("emitting on channel " + channel + " : <" + value + ">");
     socket.emit(channel, value);
     return false;
 }
     
 ////////////////////
-var init = function() {
-    populate();
-    set_url();
-}
-$(init);
+$(() => {populate(); set_url();})
