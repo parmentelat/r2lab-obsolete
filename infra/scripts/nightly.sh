4f5b245f56e823f736fdd9b6f2916de10d651743
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-11-06 14:26:39 +0100

    tweak nightly start script and cleanup topdir

--- infra/scripts/nightly.sh
+++ infra/scripts/nightly.sh
@@ -1,7 +1,4 @@
 SHELL=/bin/sh
 HOME=/root
 PATH=/root/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
-#python /root/r2lab/nightly/nightly.py -N all > /var/log/nightly.log 2>&1;
-#Now I call the fetch to check if a nightly lease is schedule once
-#the users can move it in the UI
-python /root/r2lab/nightly/fetch-nightly.py -N all > /var/log/fetch-nightly.log 2>&1;
+python3 /root/r2lab/infra/nightly/nightly.py > /var/log/nightly.log 2>&1
